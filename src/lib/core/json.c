520ce0ddd6e696926cb6cae0e4547e1509858ee1
weijing <ibeliefsky@gmail.com>
2020-01-01 22:46:09 +0800

    开始json解析

--- src/lib/core/json.c
+++ src/lib/core/json.c
@@ -3,3 +3,24 @@
 //
 
 #include "json.h"
+
+static sky_bool_t json_parse(sky_json_t *root, sky_str_t *value);
+
+sky_json_t *sky_json_load(sky_pool_t *pool, sky_str_t *value) {
+    if (sky_unlikely(!value || !value->len)) {
+        return null;
+    }
+    sky_json_t *json = sky_palloc(pool, sizeof(sky_json_t));
+    json->pool = pool;
+
+
+    return json;
+}
+
+static sky_bool_t
+json_parse(sky_json_t *root, sky_str_t *value) {
+
+    enum {
+        START = 0
+    } state;
+}
--- src/lib/core/json.h
+++ src/lib/core/json.h
@@ -5,4 +5,31 @@
 #ifndef SKY_JSON_H
 #define SKY_JSON_H
 
+#include "string.h"
+#include "palloc.h"
+
+typedef struct sky_json_s sky_json_t;
+typedef struct sky_json_node_s sky_json_node_t;
+
+typedef enum {
+    json_null,
+    json_bool,
+    json_number,
+    json_string,
+    json_object,
+    json_array
+} sky_json_type_t;
+
+struct sky_json_s {
+    sky_pool_t *pool;
+    sky_json_node_t *node;
+};
+
+struct sky_json_node_s {
+    sky_json_t *root;
+    sky_json_type_t type;
+};
+
+sky_json_t *sky_json_load(sky_pool_t *pool, sky_str_t *value);
+
 #endif //SKY_JSON_H
