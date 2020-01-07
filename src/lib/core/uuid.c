e6544eabe2cb4c7404f5d7b1cadfdf7ddd36aa1d
weijing <duan.weijing@ttwx365.com>
2020-01-07 11:23:37 +0800

    none

--- src/lib/core/uuid.c
+++ src/lib/core/uuid.c
@@ -3,3 +3,36 @@
 //
 
 #include "uuid.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void sky_uuid_generate_random(sky_uchar_t buf[37]) {
+    const sky_char_t *c = "89ab";
+    sky_uint32_t b;
+    sky_char_t *p;
+
+    p = (sky_char_t *)buf;
+    for(sky_uint8_t i = 0; i < 16; ++i) {
+        b = rand() % 255;
+        switch (i) {
+            case 6:
+                sprintf(p, "4%x", b % 15);
+                break;
+            case 8:
+                sprintf(p, "%c%x", c[rand() & 3], b % 15);
+                break;
+            default:
+                sprintf(p, "%02x", b);
+                break;
+        }
+        p += 2;
+        switch (i) {
+            case 3:
+            case 5:
+            case 9:
+                *p++ = '-';
+                break;
+        }
+    }
+    *p = '\0';
+}
--- src/lib/core/uuid.h
+++ src/lib/core/uuid.h
@@ -10,11 +10,7 @@ extern "C" {
 
 #include "types.h"
 
-typedef sky_uchar_t sky_uuid_t[16];
-
-void sky_uuid_generate(sky_uuid_t out);
-
-void sky_uuid_generate_random(sky_uuid_t out);
+void sky_uuid_generate_random(sky_uchar_t buf[37]);
 
 #if defined(__cplusplus)
 } /* extern "C" { */
