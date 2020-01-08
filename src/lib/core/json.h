e06ca1616e5f3233914f90e779df7c6ed67b9c76
weijing <duan.weijing@ttwx365.com>
2020-01-08 13:12:21 +0800

    json库引入

--- src/lib/core/json.h
+++ src/lib/core/json.h
@@ -8,32 +8,113 @@
 extern "C" {
 #endif
 
-#include "string.h"
-#include "palloc.h"
+#include "types.h"
 
-typedef struct sky_json_s sky_json_t;
-typedef struct sky_json_node_s sky_json_node_t;
+
+#ifndef json_int_t
+#define json_int_t sky_int64_t
+#endif
+
+#include <stdlib.h>
+
+typedef struct _json_value json_value;
+typedef struct _json_object_entry json_object_entry;
+
+typedef struct {
+    unsigned long max_memory;
+    int settings;
+
+    /* Custom allocator support (leave null to use malloc/free)
+     */
+
+    void *(*mem_alloc)(size_t, int zero, void *user_data);
+
+    void (*mem_free)(void *, void *user_data);
+
+    void *user_data;  /* will be passed to mem_alloc and mem_free */
+
+    size_t value_extra;  /* how much extra space to allocate for values? */
+
+} json_settings;
+
+#define json_enable_comments  0x01
 
 typedef enum {
-    json_null,
-    json_bool,
-    json_number,
-    json_string,
+    json_none,
     json_object,
-    json_array
-} sky_json_type_t;
+    json_array,
+    json_integer,
+    json_double,
+    json_string,
+    json_boolean,
+    json_null
 
-struct sky_json_s {
-    sky_pool_t *pool;
-    sky_json_node_t *node;
+} json_type;
+
+struct _json_object_entry {
+    sky_uchar_t *name;
+    unsigned int name_length;
+
+    json_value *value;
+
 };
 
-struct sky_json_node_s {
-    sky_json_t *root;
-    sky_json_type_t type;
+struct _json_value {
+    json_value *parent;
+
+    json_type type;
+
+    union {
+        sky_bool_t boolean;
+        json_int_t integer;
+        double dbl;
+
+        struct {
+            unsigned int length;
+            sky_uchar_t *ptr; /* null terminated */
+
+        } string;
+
+        struct {
+            unsigned int length;
+
+            json_object_entry *values;
+
+        } object;
+
+        struct {
+            unsigned int length;
+            json_value **values;
+        } array;
+
+    } u;
+
+    union {
+        json_value *next_alloc;
+        void *object_mem;
+
+    } _reserved;
 };
 
-sky_json_t *sky_json_load(sky_pool_t *pool, sky_str_t *value);
+json_value *json_parse(const sky_uchar_t *json,
+                       size_t length);
+
+#define json_error_max 128
+
+json_value *json_parse_ex(json_settings *settings,
+                          const sky_uchar_t *json,
+                          size_t length,
+                          char *error);
+
+void json_value_free(json_value *);
+
+
+/* Not usually necessary, unless you used a custom mem_alloc and now want to
+ * use a custom mem_free.
+ */
+void json_value_free_ex(json_settings *settings,
+                        json_value *);
+
 
 #if defined(__cplusplus)
 } /* extern "C" { */
