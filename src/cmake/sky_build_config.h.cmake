d74e01501837d210ab14d91b581f12857357a0e4
weijing <duan.weijing@ttwx365.com>
2020-01-20 13:14:09 +0800

    none

--- src/cmake/sky_build_config.h.cmake
+++ src/cmake/sky_build_config.h.cmake
@@ -4,19 +4,29 @@
 #define SKY_BUILD_CONFIG_H
 
 /* API available in Glibc/Linux, but possibly not elsewhere */
-#cmakedefine HAS_ACCEPT4
-#cmakedefine HAS_ALLOCA_H
-#cmakedefine HAS_CLOCK_GETTIME
-#cmakedefine HAS_GET_CURRENT_DIR_NAME
-#cmakedefine HAS_GETAUXVAL
-#cmakedefine HAS_MEMPCPY
-#cmakedefine HAS_MEMRCHR
-#cmakedefine HAS_MKOSTEMP
-#cmakedefine HAS_PIPE2
-#cmakedefine HAS_PTHREADBARRIER
-#cmakedefine HAS_RAWMEMCHR
-#cmakedefine HAS_READAHEAD
-#cmakedefine HAS_REALLOCARRAY
+#cmakedefine HAVE_ACCEPT4
+#cmakedefine HAVE_ALLOCA_H
+#cmakedefine HAVE_CLOCK_GETTIME
+#cmakedefine HAVE_GET_CURRENT_DIR_NAME
+#cmakedefine HAVE_GETAUXVAL
+#cmakedefine HAVE_MEMPCPY
+#cmakedefine HAVE_MEMRCHR
+#cmakedefine HAVE_MKOSTEMP
+#cmakedefine HAVE_PIPE2
+#cmakedefine HAVE_PTHREADBARRIER
+#cmakedefine HAVE_RAWMEMCHR
+#cmakedefine HAVE_READAHEAD
+#cmakedefine HAVE_REALLOCARRAY
+#cmakedefine HAVE_EVENTFD
+#cmakedefine HAVE_EPOLL
+#cmakedefine HAVE_KQUEUE
+#cmakedefine HAVE_DLADDR
+#cmakedefine HAVE_POSIX_FADVISE
+#cmakedefine HAVE_LINUX_CAPABILITY
+#cmakedefine HAVE_PTHREAD_SET_NAME_NP
+#cmakedefine HAVE_GETENTROPY
+#cmakedefine HAVE_FWRITE_UNLOCKED
+#cmakedefine HAVE_GETTID
 
 /* Compiler builtins for specific CPU instruction support */
 #cmakedefine HAVE_BUILTIN_CLZLL
@@ -24,16 +34,18 @@
 #cmakedefine HAVE_BUILTIN_IA32_CRC32
 #cmakedefine HAVE_BUILTIN_MUL_OVERFLOW
 #cmakedefine HAVE_BUILTIN_ADD_OVERFLOW
-#cmakedefine HAVE_BUILTIN_BSWAP
+#cmakedefine HAVE_BUILTIN_FPCLASSIFY
 
 /* C11 _Static_assert() */
 #cmakedefine HAVE_STATIC_ASSERT
 
 /* Libraries */
 #cmakedefine HAVE_LUA
+#cmakedefine HAVE_BROTLI
+#cmakedefine HAVE_ZSTD
 
 /* Valgrind support for coroutines */
-#cmakedefine USE_VALGRIND
+#cmakedefine HAVE_VALGRIND
 
 #endif
 
