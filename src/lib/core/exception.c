a2848e11ed14cfbe45bf194b04c2630595ac3e27
weijing <ibeliefsky@gmail.com>
2020-01-25 10:53:21 +0800

    none

---
+++ src/lib/core/exception.c
@@ -0,0 +1,5 @@
+//
+// Created by weijing on 2020/1/23.
+//
+
+#include "exception.h"
---
+++ src/lib/core/exception.h
@@ -0,0 +1,20 @@
+//
+// Created by weijing on 2020/1/23.
+//
+
+#ifndef SKY_EXCEPTION_H
+#define SKY_EXCEPTION_H
+
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+typedef struct {
+
+} sky_exception_t;
+
+#if defined(__cplusplus)
+} /* extern "C" { */
+#endif
+#endif //SKY_EXCEPTION_H
