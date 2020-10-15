7e06153e21bb02ef710f906d41db93ffb7f93db8
weijing <duan.weijing@ttwx365.com>
2020-10-15 16:02:08 +0800

    none

--- src/lib/core/cpuinfo.h
+++ src/lib/core/cpuinfo.h
@@ -17,13 +17,16 @@ extern "C" {
 
 typedef cpu_set_t sky_cpu_set_t;
 
-#define sky_setaffinity(_c)   sched_setaffinity(0, sizeof(sky_cpu_set_t), _c)
-#elif defined(__FreeBSD__) || defined(__APPLE__)
-#include <sys/param.h>
-#include <sys/cpuset.h>
-typedef cpuset_t sky_cpu_set_t;
 #define sky_setaffinity(_c) \
-    cpuset_setaffinity(CPU_LEVEL_WHICH, CPU_WHICH_PID, -1, sizeof(sky_cpu_set_t), _c)
+    sched_setaffinity(0, sizeof(sky_cpu_set_t), _c)
+#elif defined(__FreeBSD__) || defined(__APPLE__)
+
+#include <sys/cpuset.h>
+
+typedef cpuset_t sky_cpu_set_t;
+
+#define sky_setaffinity(_c) \
+    cpuset_setaffinity(CPU_LEVEL_WHICH, CPU_WHICH_PID, -1, sizeof(cpuset_t), _c)
 #endif
 
 extern sky_uint_t sky_cache_line_size;
