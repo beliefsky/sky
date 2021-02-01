5dd40dce56da5dd89fc1ae950fb0298cab831cd3
weijing <duanweijing@deltaphone.com.cn>
2021-02-01 13:31:03 +0800

    none

--- src/lib/net/http/http_io_wrappers.h
+++ src/lib/net/http/http_io_wrappers.h
@@ -7,6 +7,10 @@
 
 #include "http_server.h"
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 sky_size_t http_read(sky_http_connection_t *conn, sky_uchar_t *data, sky_size_t size);
 
 void http_write(sky_http_connection_t *conn, const sky_uchar_t *data, sky_size_t size);
@@ -15,4 +19,7 @@ void
 http_send_file(sky_http_connection_t *conn, sky_int32_t fd, sky_int64_t offset, sky_size_t size,
                const sky_uchar_t *header, sky_uint32_t header_len);
 
+#if defined(__cplusplus)
+} /* extern "C" { */
+#endif
 #endif //SKY_HTTP_IO_WRAPPERS_H
