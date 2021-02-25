599e9f426166beab6c60b168acb25f5ad9b26456
weijing <duanweijing@deltaphone.com.cn>
2021-02-25 13:53:01 +0800

    none

--- src/lib/net/clients/tcp_client.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by edz on 2021/2/25.
-//
-
-#include "tcp_client.h"
--- src/lib/net/clients/tcp_client.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by edz on 2021/2/25.
-//
-
-#ifndef SKY_TCP_CLIENT_H
-#define SKY_TCP_CLIENT_H
-
-#endif //SKY_TCP_CLIENT_H
