20960acd1dcccdd7f50eac092b6b9b31882e61f7
beliefsky <ibeliefsky@gmail.com>
2021-07-19 21:18:58 +0800

    重写hash map准备

---
+++ src/lib/core/hash_map.h
@@ -0,0 +1,22 @@
+//
+// Created by weijing on 17-11-24.
+//
+
+#ifndef SKY_HASH_MAP_H
+#define SKY_HASH_MAP_H
+
+#include "types.h"
+#include "string.h"
+#include "palloc.h"
+#include "array.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+
+
+#if defined(__cplusplus)
+} /* extern "C" { */
+#endif
+#endif //SKY_HASH_MAP_H
