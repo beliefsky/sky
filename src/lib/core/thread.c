abdc7ebea4fcf7efbcc26f7d50cedfbbcbd907c2
weijing <duanweijing@deltaphone.com.cn>
2021-11-04 14:58:09 +0800

    多线程api

--- src/lib/core/thread.c
+++ src/lib/core/thread.c
@@ -2,4 +2,17 @@
 // Created by edz on 2021/11/4.
 //
 
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include "thread.h"
+
+void
+sky_thread_set_cpu(sky_thread_t thread, sky_i32_t n) {
+    cpu_set_t set;
+
+    CPU_ZERO(&set);
+    CPU_SET(n, &set);
+    pthread_setaffinity_np(thread, sizeof(cpu_set_t), &set);
+}
--- src/lib/core/thread.h
+++ src/lib/core/thread.h
@@ -12,9 +12,48 @@
 extern "C" {
 #endif
 
-#define sky_thread __thread
+#define SKY_THREAD_SCOPE_SYSTEM PTHREAD_SCOPE_SYSTEM
+#define SKY_THREAD_SCOPE_PROCESS PTHREAD_SCOPE_PROCESS
 
 
+typedef pthread_t sky_thread_t;
+typedef pthread_attr_t sky_thread_attr_t;
+
+typedef void *(*sky_thread_pt)(void *);
+
+
+static sky_inline void
+sky_thread_create(sky_thread_t *thread, sky_thread_attr_t *attr, sky_thread_pt handle, void *data) {
+    pthread_create(thread, attr, handle, data);
+}
+
+void sky_thread_set_cpu(sky_thread_t thread, sky_i32_t n);
+
+static sky_inline void
+sky_thread_join(sky_thread_t thread, void **data_ptr) {
+    pthread_join(thread, data_ptr);
+}
+
+
+static sky_inline void
+sky_thread_attr_init(sky_thread_attr_t *attr) {
+    pthread_attr_init(attr);
+}
+
+static sky_inline void
+sky_thread_attr_set_scope(sky_thread_attr_t *attr, sky_i32_t scope) {
+    pthread_attr_setscope(attr, scope);
+}
+
+static sky_inline void
+sky_thread_attr_set_stack_size(sky_thread_attr_t *attr, sky_usize_t size) {
+    pthread_attr_setstacksize(attr, size);
+}
+
+static sky_inline void
+sky_thread_attr_destroy(sky_thread_attr_t *attr) {
+    pthread_attr_destroy(attr);
+}
 
 
 #if defined(__cplusplus)
