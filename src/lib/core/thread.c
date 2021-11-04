c0ce4f860df254a5314796e1111f1568f458dec6
weijing <duanweijing@deltaphone.com.cn>
2021-11-04 15:17:42 +0800

    线程亲和力兼容多平台

--- src/lib/core/thread.c
+++ src/lib/core/thread.c
@@ -8,11 +8,27 @@
 
 #include "thread.h"
 
+#if defined(__FreeBSD__)
+
+#include <sys/param.h>
+#include <sys/cpuset.h>
+#endif
+
+
 void
 sky_thread_set_cpu(sky_thread_t thread, sky_i32_t n) {
+#if defined(__linux__)
+
     cpu_set_t set;
 
     CPU_ZERO(&set);
     CPU_SET(n, &set);
     pthread_setaffinity_np(thread, sizeof(cpu_set_t), &set);
+#elif defined(__FreeBSD__)
+    cpuset_t set;
+
+    CPU_ZERO(&set);
+    CPU_SET(n, &set);
+    pthread_setaffinity_np(thread, sizeof(cpu_set_t), &set);
+#endif
 }
