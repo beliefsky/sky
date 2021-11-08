2ef3c093a3e31a847aaa2b248523ccfd3fcfc3d2
weijing <duanweijing@deltaphone.com.cn>
2021-11-08 16:22:52 +0800

    none

--- src/lib/core/thread.c
+++ src/lib/core/thread.c
@@ -2,9 +2,6 @@
 // Created by edz on 2021/11/4.
 //
 
-#ifndef _GNU_SOURCE
-#define _GNU_SOURCE
-#endif
 
 #include "thread.h"
 
@@ -18,7 +15,6 @@
 void
 sky_thread_set_cpu(sky_thread_t thread, sky_i32_t n) {
 #if defined(__linux__)
-
     cpu_set_t set;
 
     CPU_ZERO(&set);
--- src/lib/core/thread.h
+++ src/lib/core/thread.h
@@ -5,6 +5,10 @@
 #ifndef SKY_THREAD_H
 #define SKY_THREAD_H
 
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include <pthread.h>
 #include "types.h"
 
