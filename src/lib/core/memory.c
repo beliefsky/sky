576bde0ad71847aa235bca5f5bbca3779a0b0228
weijing <duanweijing@deltaphone.com.cn>
2021-12-07 16:12:47 +0800

    内存申请和是否时不再使用宏定义，方便后期内存管理库依赖

---
+++ src/lib/core/memory.c
@@ -0,0 +1,19 @@
+//
+// Created by edz on 2021/12/7.
+//
+#include "memory.h"
+
+sky_inline void *
+sky_malloc(sky_usize_t size) {
+    return malloc(size);
+}
+
+sky_inline void
+sky_free(void *ptr) {
+    free(ptr);
+}
+
+sky_inline void *
+sky_realloc(void *ptr, sky_usize_t resize) {
+    return realloc(ptr, resize);
+}
