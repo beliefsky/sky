4ba9d994c0d1d63e2e015e833c64a00b36c1b888
weijing <duanweijing@deltaphone.com.cn>
2021-12-09 16:49:08 +0800

    直接单个头文件

--- src/lib/core/queue.c
+++ src/lib/core/queue.c
@@ -3,32 +3,3 @@
 //
 
 #include "queue.h"
-
-sky_inline void
-sky_queue_insert_next_list(sky_queue_t *queue, sky_queue_t *link) {
-    if (sky_unlikely(sky_queue_is_empty(link))) {
-        return;
-    }
-    link->next->prev = queue;
-    link->prev->next = queue->next;
-
-    queue->next->prev = link->prev;
-    queue->next = link->next;
-
-    sky_queue_init(link);
-}
-
-sky_inline void
-sky_queue_insert_prev_list(sky_queue_t *queue, sky_queue_t *link) {
-    if (sky_unlikely(sky_queue_is_empty(link))) {
-        return;
-    }
-    link->prev->next = queue;
-    link->next->prev = queue->prev;
-
-    queue->prev->next = link->next;
-    queue->prev = link->prev;
-
-    sky_queue_init(link);
-
-}
