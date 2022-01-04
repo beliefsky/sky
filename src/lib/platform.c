84e10e7722593d62099f4be5dfd7554dd9230193
weijing <duanweijing@deltaphone.com.cn>
2022-01-04 14:52:32 +0800

    platform 托管event loop, 方便后期多线程数据分发推送

--- src/lib/platform.c
+++ src/lib/platform.c
@@ -17,10 +17,22 @@
 #include <pthread_np.h>
 #endif
 
+typedef struct {
+    sky_u32_t index;
+    pthread_t thread;
+    sky_platform_t *parent;
+    sky_event_loop_t *loop;
+} thread_item_t;
+
 struct sky_platform_s {
+    sky_bool_t cpu_bind;
     sky_u32_t thread_n;
-    pthread_t *thread;
+    sky_platform_run_pt run;
+    sky_platform_destroy_pt destroy;
+    thread_item_t *items;
 };
+
+static void *thread_run(void *data);
 
 static void thread_bind_cpu(pthread_t thread, sky_i32_t n);
 
@@ -28,7 +40,8 @@ sky_platform_t *
 sky_platform_create(const sky_platform_conf_t *conf) {
     sky_bool_t cpu_bind;
     sky_u32_t thread_size;
-    pthread_t *thread;
+    sky_platform_t *platform;
+    thread_item_t *item;
 
     if (!conf->run) {
         return null;
@@ -45,40 +58,76 @@ sky_platform_create(const sky_platform_conf_t *conf) {
         thread_size = (sky_u32_t) size;
     }
 
-    sky_platform_t *platform = sky_malloc(
-            sizeof(sky_platform_t) + (sizeof(pthread_t) * (sky_usize_t) thread_size));
+    platform = sky_malloc(sizeof(sky_platform_t) + (sizeof(thread_item_t) * (sky_usize_t) thread_size));
+    item = (thread_item_t *) (platform + 1);
+
+    platform->cpu_bind = cpu_bind;
     platform->thread_n = thread_size;
-    platform->thread = (pthread_t *) (platform + 1);
+    platform->run = conf->run;
+    platform->destroy = conf->destroy;
+    platform->items = item;
 
-    pthread_attr_t attr;
-    for (sky_u32_t i = 0; i < platform->thread_n; ++i) {
-        thread = platform->thread + i;
 
-        pthread_attr_init(&attr);
-        pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
-        pthread_attr_setstacksize(&attr, 32768);
-
-        pthread_create(thread, &attr, (void *(*)(void *)) conf->run, (void *) (sky_usize_t) i);
-        if (cpu_bind) {
-            thread_bind_cpu(*thread, (sky_i32_t) i);
-        }
-        pthread_attr_destroy(&attr);
+    for (sky_u32_t i = 0; i < thread_size; ++i, ++item) {
+        item->index = i;
+        item->parent = platform;
+        item->loop = sky_event_loop_create();
     }
+
 
     return platform;
 }
 
 void
-sky_platform_wait(sky_platform_t *platform) {
-    for (sky_u32_t i = 0; i < platform->thread_n; ++i) {
+sky_platform_run(sky_platform_t *platform) {
+    sky_u32_t i;
+    thread_item_t *item;
+    pthread_attr_t attr;
 
-        pthread_join(platform->thread[i], null);
+    item = platform->items;
+    for (i = 0; i < platform->thread_n; ++i, ++item) {
+        pthread_attr_init(&attr);
+        pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+        pthread_attr_setstacksize(&attr, 32768);
+
+        pthread_create(&item->thread, &attr, thread_run, item);
+        if (platform->cpu_bind) {
+            thread_bind_cpu(item->thread, (sky_i32_t) i);
+        }
+        pthread_attr_destroy(&attr);
+    }
+
+    item = platform->items;
+    for (i = 0; i < platform->thread_n; ++i, ++item) {
+        pthread_join(item->thread, null);
     }
 }
 
 void
 sky_platform_destroy(sky_platform_t *platform) {
     sky_free(platform);
+}
+
+static void *
+thread_run(void *data) {
+    thread_item_t *item = data;
+    void *result = null;
+    sky_platform_run_pt handle = item->parent->run;
+
+    if (sky_likely(handle)) {
+        result = handle(item->loop, item->index);
+    }
+
+    sky_event_loop_run(item->loop);
+
+    sky_platform_destroy_pt destroy = item->parent->destroy;
+    if (sky_likely(destroy)) {
+        destroy(item->loop, result);
+    }
+
+    sky_event_loop_destroy(item->loop);
+
+    return null;
 }
 
 
--- src/lib/platform.h
+++ src/lib/platform.h
@@ -6,6 +6,7 @@
 #define SKY_PLATFORM_H
 
 #include "core/types.h"
+#include "event/event_loop.h"
 
 #if defined(__cplusplus)
 extern "C" {
@@ -13,18 +14,20 @@ extern "C" {
 
 typedef struct sky_platform_s sky_platform_t;
 
-typedef void (*sky_platform_pt)(sky_u32_t index);
+typedef void *(*sky_platform_run_pt)(sky_event_loop_t *loop, sky_u32_t index);
+
+typedef void (*sky_platform_destroy_pt)(sky_event_loop_t *loop, void *data);
 
 typedef struct {
     sky_u32_t thread_size;
-    sky_platform_pt run;
-    void *data;
+    sky_platform_run_pt run;
+    sky_platform_destroy_pt destroy;
 } sky_platform_conf_t;
 
 
 sky_platform_t *sky_platform_create(const sky_platform_conf_t *conf);
 
-void sky_platform_wait(sky_platform_t *platform);
+void sky_platform_run(sky_platform_t *platform);
 
 void sky_platform_destroy(sky_platform_t *platform);
 
