9bb28f444020bb6e501767bfcffda49c22d24fb4
beliefsky <ibeliefsky@gmail.com>
2022-02-21 18:51:59 +0800

    ET模式，尽量写入

--- src/lib/net/mqtt/mqtt_response.c
+++ src/lib/net/mqtt/mqtt_response.c
@@ -5,6 +5,8 @@
 #include "mqtt_response.h"
 #include "../../core/memory.h"
 
+static sky_bool_t mqtt_write_packet(sky_mqtt_connect_t *conn);
+
 
 void
 sky_mqtt_send_connect_ack(sky_mqtt_connect_t *conn, sky_bool_t session_preset, sky_u8_t status) {
@@ -16,6 +18,7 @@ sky_mqtt_send_connect_ack(sky_mqtt_connect_t *conn, sky_bool_t session_preset, s
     packet->size = sky_mqtt_connect_ack_unpack(packet->data, session_preset, status);
 
     sky_queue_insert_prev(&conn->packet, &packet->link);
+    mqtt_write_packet(conn);
 }
 
 void
@@ -29,6 +32,7 @@ sky_mqtt_send_publish(sky_mqtt_connect_t *conn, const sky_mqtt_publish_msg_t *ms
     packet->size = sky_mqtt_publish_unpack(packet->data, msg, qos, retain, dup);
 
     sky_queue_insert_prev(&conn->packet, &packet->link);
+    mqtt_write_packet(conn);
 }
 
 void
@@ -41,6 +45,7 @@ sky_mqtt_send_publish_ack(sky_mqtt_connect_t *conn, sky_u16_t packet_identifier)
     packet->size = sky_mqtt_publish_ack_unpack(packet->data, packet_identifier);
 
     sky_queue_insert_prev(&conn->packet, &packet->link);
+    mqtt_write_packet(conn);
 }
 
 void
@@ -53,6 +58,7 @@ sky_mqtt_send_publish_rec(sky_mqtt_connect_t *conn, sky_u16_t packet_identifier)
     packet->size = sky_mqtt_publish_rec_unpack(packet->data, packet_identifier);
 
     sky_queue_insert_prev(&conn->packet, &packet->link);
+    mqtt_write_packet(conn);
 }
 
 void
@@ -65,6 +71,7 @@ sky_mqtt_send_publish_rel(sky_mqtt_connect_t *conn, sky_u16_t packet_identifier)
     packet->size = sky_mqtt_publish_rel_unpack(packet->data, packet_identifier);
 
     sky_queue_insert_prev(&conn->packet, &packet->link);
+    mqtt_write_packet(conn);
 }
 
 void
@@ -77,6 +84,7 @@ sky_mqtt_send_publish_comp(sky_mqtt_connect_t *conn, sky_u16_t packet_identifier
     packet->size = sky_mqtt_publish_comp_unpack(packet->data, packet_identifier);
 
     sky_queue_insert_prev(&conn->packet, &packet->link);
+    mqtt_write_packet(conn);
 }
 
 void
@@ -90,6 +98,7 @@ sky_mqtt_send_sub_ack(sky_mqtt_connect_t *conn, sky_u16_t packet_identifier,
     packet->size = sky_mqtt_sub_ack_unpack(packet->data, packet_identifier, max_qos, topic_num);
 
     sky_queue_insert_prev(&conn->packet, &packet->link);
+    mqtt_write_packet(conn);
 }
 
 void
@@ -102,6 +111,7 @@ sky_mqtt_send_unsub_ack(sky_mqtt_connect_t *conn, sky_u16_t packet_identifier) {
     packet->size = sky_mqtt_unsub_ack_unpack(packet->data, packet_identifier);
 
     sky_queue_insert_prev(&conn->packet, &packet->link);
+    mqtt_write_packet(conn);
 }
 
 void
@@ -114,4 +124,40 @@ sky_mqtt_send_ping_resp(sky_mqtt_connect_t *conn) {
     packet->size = sky_mqtt_ping_resp_unpack(packet->data);
 
     sky_queue_insert_prev(&conn->packet, &packet->link);
+    mqtt_write_packet(conn);
+}
+
+static sky_bool_t
+mqtt_write_packet(sky_mqtt_connect_t *conn) {
+    sky_mqtt_packet_t *packet;
+    sky_uchar_t *buf;
+    sky_isize_t size;
+
+    if (sky_event_none_write(&conn->ev)) {
+        return true;
+    }
+    while (!sky_queue_is_empty(&conn->packet)) {
+        packet = (sky_mqtt_packet_t *) sky_queue_next(&conn->packet);
+
+        buf = packet->data + conn->write_size;
+
+        for (;;) {
+            size = conn->server->mqtt_write_nowait(conn, buf, packet->size - conn->write_size);
+            if (sky_unlikely(size == -1)) {
+                return false;
+            } else if (size == 0) {
+                return true;
+            }
+            conn->write_size += size;
+            buf += size;
+            if (conn->write_size >= packet->size) {
+                break;
+            }
+        }
+        conn->write_size = 0;
+        sky_queue_remove(&packet->link);
+        sky_free(packet);
+    }
+
+    return true;
 }
