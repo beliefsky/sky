d16573d4b2e0f53bedc96c8cb708807c2bf201c9
weijing <duanweijing@deltaphone.com.cn>
2022-02-22 14:58:14 +0800

    topic tree 作为基础算法进行维护

---
+++ src/lib/core/topic_tree.c
@@ -0,0 +1,5 @@
+//
+// Created by edz on 2022/2/22.
+//
+
+#include "topic_tree.h"
---
+++ src/lib/core/topic_tree.h
@@ -0,0 +1,13 @@
+//
+// Created by edz on 2022/2/22.
+//
+
+#ifndef SKY_TOPIC_TREE_H
+#define SKY_TOPIC_TREE_H
+
+
+struct sky_topic_tree_s {
+
+};
+
+#endif //SKY_TOPIC_TREE_H
