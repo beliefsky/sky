8850d275872ceeb8cf34ee895f5c958c5d93ce1e
beliefsky <ibeliefsky@gmail.com>
2022-02-22 23:08:44 +0800

    hash map

---
+++ src/lib/core/hashmap.c
@@ -0,0 +1,5 @@
+//
+// Created by beliefsky on 2022/2/22.
+//
+
+#include "hashmap.h"
---
+++ src/lib/core/hashmap.h
@@ -0,0 +1,8 @@
+//
+// Created by beliefsky on 2022/2/22.
+//
+
+#ifndef SKY_HASHMAP_H
+#define SKY_HASHMAP_H
+
+#endif //SKY_HASHMAP_H
