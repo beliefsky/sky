fa0b95c0ef25449594dfaa1c94dce19fec0445e5
beliefsky <ibeliefsky@gmail.com>
2022-02-23 00:32:22 +0800

    none

--- src/lib/core/hashmap.c
+++ src/lib/core/hashmap.c
@@ -5,6 +5,7 @@
 #include "hashmap.h"
 #include "memory.h"
 
+
 typedef struct {
     sky_u64_t hash: 48;
     sky_u64_t dib: 16;
@@ -16,11 +17,15 @@ struct sky_hashmap_s {
     bucket_t e_data;
     sky_hashmap_hash_pt hash;
     sky_hashmap_cmp_pt cmp;
+    sky_usize_t count;
     sky_usize_t cap;
     sky_usize_t bucket_num;
     sky_usize_t mask;
     bucket_t *buckets;
 };
+
+#define get_hash(_map, _item) \
+    ((_map)->hash(_item) << 16 >> 16)
 
 sky_hashmap_t *
 sky_hashmap_create(sky_hashmap_hash_pt hash, sky_hashmap_cmp_pt cmp) {
@@ -33,22 +38,35 @@ sky_hashmap_create(sky_hashmap_hash_pt hash, sky_hashmap_cmp_pt cmp) {
     map->e_data.data = null;
     map->hash = hash;
     map->cmp = cmp;
+    map->count = 0;
     map->cap = 16;
     map->bucket_num = map->cap;
     map->mask = map->bucket_num - 1;
     map->buckets = sky_malloc(sizeof(bucket_t) * map->bucket_num);
+    sky_memzero(map->buckets, sizeof(bucket_t) * map->bucket_num);
 
     return map;
 }
 
 void *
 sky_hashmap_put(sky_hashmap_t *map, void *item) {
+    if (sky_unlikely(map->count == map->bucket_num)) {
+        // resize
+    }
 
     return null;
 }
 
 void *
 sky_hashmap_get(sky_hashmap_t *map, void *item) {
+    bucket_t *entry = &map->e_data;
+    entry->hash = get_hash(map, item);
+    entry->dib = 1;
+    entry->data = item;
+
+    sky_usize_t i = entry->hash & map->mask;
+
+
 
     return null;
 }
