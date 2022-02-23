6e6b65dd93ef139569f003b741f3bf3c8f04155c
beliefsky <ibeliefsky@gmail.com>
2022-02-23 22:20:03 +0800

    立项: 优秀的hash待选方案

---
+++ src/lib/core/hash.c
@@ -0,0 +1,5 @@
+//
+// Created by beliefsky on 2022/2/23.
+//
+
+#include "hash.h"
---
+++ src/lib/core/hash.h
@@ -0,0 +1,17 @@
+//
+// Created by beliefsky on 2022/2/23.
+//
+
+#ifndef SKY_HASH_H
+#define SKY_HASH_H
+#include "types.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#if defined(__cplusplus)
+} /* extern "C" { */
+#endif
+
+#endif //SKY_HASH_H
