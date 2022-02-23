65dd97ff4ff986c62ba2efe2cdc159da6a7db208
beliefsky <ibeliefsky@gmail.com>
2022-02-23 23:53:54 +0800

    wyhash 引入

--- src/lib/net/inet.h
+++ src/lib/net/inet.h
@@ -8,29 +8,6 @@
 #include "../core/types.h"
 #include <sys/socket.h>
 
-#ifdef HAVE_BUILTIN_BSWAP
-#define sky_swap_u16(_ll) __builtin_bswap16(_ll)
-#define sky_swap_u32(_ll) __builtin_bswap32(_ll)
-#define sky_swap_u64(_ll) __builtin_bswap64(_ll)
-
-#else
-#define sky_swap_u16(_s)    (sky_u16_t)(((_s) & 0x00FF) << 8 | ((_s) & 0xFF00) >> 8)
-#define sky_swap_u32(_l)    (sky_u32_t)  \
-    (((_l) & 0x000000FF) << 24 |            \
-    ((_l) & 0x0000FF00) << 8  |             \
-    ((_l) & 0x00FF0000) >> 8  |             \
-    ((_l) & 0xFF000000) >> 24)
-
-#define sky_swap_u64(_ll)   (sky_u64_t)  \
-    (((_ll) & 0x00000000000000FF) << 56 |   \
-    ((_ll) & 0x000000000000FF00) << 40 |    \
-    ((_ll) & 0x0000000000FF0000) << 24 |    \
-    ((_ll) & 0x00000000FF000000) << 8  |    \
-    ((_ll) & 0x000000FF00000000) >> 8  |    \
-    ((_ll) & 0x0000FF0000000000) >> 24 |    \
-    ((_ll) & 0x00FF000000000000) >> 40 |    \
-    ((_ll) & 0xFF00000000000000) >> 56)
-#endif
 #if __BYTE_ORDER == __LITTLE_ENDIAN
 #define sky_htons(_s)   sky_swap_u16(_s)
 #define sky_ntohs(_s)   sky_swap_u16(_s)
