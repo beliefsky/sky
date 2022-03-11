f100a7a1289b0eb4bfe56eb26291c19636559780
weijing <duanweijing@deltaphone.com.cn>
2022-03-11 14:47:16 +0800

    纯http 可关闭 线程消息

--- src/tests/sky_test_http_file.c
+++ src/tests/sky_test_http_file.c
@@ -16,7 +16,10 @@ main() {
     setvbuf(stdout, null, _IOLBF, 0);
     setvbuf(stderr, null, _IOLBF, 0);
 
-    sky_event_manager_t *manager = sky_event_manager_create();
+    const sky_event_manager_conf_t conf = {
+            .enable_share = false,
+    };
+    sky_event_manager_t *manager = sky_event_manager_create_with_conf(&conf);
 
     create_server(manager);
 
