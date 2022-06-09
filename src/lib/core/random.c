14f246eea56e1d631bf09a0ebe18251a99a4c910
weijing <duanweijing@deltaphone.com.cn>
2022-06-09 10:34:01 +0800

    none

--- src/lib/core/random.c
+++ src/lib/core/random.c
@@ -4,38 +4,8 @@
 
 #include "random.h"
 
-#if defined(__WINNT__)
-#include "../core/memory.h"
-#include <stdlib.h>
-
-#else
 #include <fcntl.h>
 #include <unistd.h>
-
-#endif
-
-sky_bool_t
-sky_random_bytes(sky_uchar_t *in, sky_u32_t size) {
-    sky_i32_t value;
-
-    while (size >= 4) {
-        value = rand();
-        sky_memcpy4(in, &value);
-
-        size -= 4;
-        in += 4;
-    }
-    if (size > 0) {
-        value = rand();
-        sky_memcpy(in, &value, size);
-    }
-
-    return true;
-}
-
-#if defined(__WINNT__)
-
-#else
 
 sky_bool_t
 sky_random_bytes(sky_uchar_t *in, sky_u32_t size) {
@@ -43,8 +13,6 @@ sky_random_bytes(sky_uchar_t *in, sky_u32_t size) {
     if (sky_unlikely(fd < 0)) {
         return false;
     }
-    rand()
-
     if (sky_unlikely(read(fd, in, size) < 0)) {
         close(fd);
         return false;
@@ -53,5 +21,3 @@ sky_random_bytes(sky_uchar_t *in, sky_u32_t size) {
 
     return true;
 }
-
-#endif
