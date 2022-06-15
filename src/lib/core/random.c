ed18507d8b5d2f39bd27da3e6cbdd074750d73e1
weijing <duanweijing@deltaphone.com.cn>
2022-06-15 14:40:52 +0800

    cmake 动态指定 event_loop

--- src/lib/core/random.c
+++ src/lib/core/random.c
@@ -9,7 +9,11 @@
 
 sky_bool_t
 sky_random_bytes(sky_uchar_t *in, sky_u32_t size) {
+#ifdef O_CLOEXEC
     sky_i32_t fd = open("/dev/urandom", O_RDONLY | O_CLOEXEC);
+#else
+    sky_i32_t fd = open("/dev/urandom", O_RDONLY);
+#endif
     if (sky_unlikely(fd < 0)) {
         return false;
     }
