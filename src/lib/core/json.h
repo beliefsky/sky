bf3b41f47ad8e52b6bda11c00c9bf2100a5801df
weijing <duanweijing@deltaphone.com.cn>
2022-10-25 18:02:57 +0800

    定时提交

--- src/lib/core/json.h
+++ src/lib/core/json.h
@@ -13,32 +13,101 @@
 extern "C" {
 #endif
 
+/** Default option (RFC 8259 compliant):
+- Read positive integer as uint64_t.
+- Read negative integer as int64_t.
+- Read floating-point number as double with round-to-nearest mode.
+- Read integer which cannot fit in uint64_t or int64_t as double.
+- Report error if real number is infinity.
+- Report error if string contains invalid UTF-8 character or BOM.
+- Report error on trailing commas, comments, inf and nan literals. */
+#define SKY_JSON_READ_NO_FLAG SKY_U32(0)
+
+/** Read the input data in-situ.
+    This option allows the reader to modify and use input data to store string
+    values, which can increase reading speed slightly.
+    The caller should hold the input data before free the document.
+    The input data must be padded by at least `YYJSON_PADDING_SIZE` bytes.
+    For example: "[1,2]" should be "[1,2]\0\0\0\0", length should be 5. */
+#define SKY_JSON_READ_IN_SITU (SKY_U32(1) << 0)
+
+/** Stop when done instead of issuing an error if there's additional content
+    after a JSON document. This option may be used to parse small pieces of JSON
+    in larger data, such as `NDJSON`. */
+#define SKY_JSON_READ_STOP_WHEN_DONE    (SKY_U32(1)  << 1)
+
+/** Allow single trailing comma at the end of an object or array,
+    such as [1,2,3,] {"a":1,"b":2,} (non-standard). */
+#define SKY_JSON_READ_ALLOW_TRAILING_COMMAS (SKY_U32(1) << 2)
+
+/** Allow C-style single line and multiple line comments (non-standard). */
+#define SKY_JSON_READ_ALLOW_COMMENTS    (SKY_U32(1)  << 3)
+
+/** Allow inf/nan number and literal, case-insensitive,
+    such as 1e999, NaN, inf, -Infinity (non-standard). */
+#define SKY_JSON_READ_ALLOW_INF_AND_NAN (SKY_U32(1)  << 4)
+
+/** Read number as raw string (value with YYJSON_TYPE_RAW type),
+    inf/nan literal is also read as raw with `ALLOW_INF_AND_NAN` flag. */
+#define SKY_JSON_READ_NUMBER_AS_RAW (SKY_U32(1)  << 5)
+
+/** Allow reading invalid unicode when parsing string values (non-standard).
+    Invalid characters will be allowed to appear in the string values, but
+    invalid escape sequences will still be reported as errors.
+    This flag does not affect the performance of correctly encoded strings.
+
+    @warning Strings in JSON values may contain incorrect encoding when this
+    option is used, you need to handle these strings carefully to avoid security
+    risks. */
+#define SKY_JSON_READ_ALLOW_INVALID_UNICODE (SKY_U32(1)  << 6)
+
+
+/** Type of JSON value (3 bit). */
+#define SKY_JSON_TYPE_NONE        SKY_U8(0)        /* _____000 */
+#define SKY_JSON_TYPE_RAW         SKY_U8(1)        /* _____001 */
+#define SKY_JSON_TYPE_NULL        SKY_U8(2)        /* _____010 */
+#define SKY_JSON_TYPE_BOOL        SKY_U8(3)        /* _____011 */
+#define SKY_JSON_TYPE_NUM         SKY_U8(4)        /* _____100 */
+#define SKY_JSON_TYPE_STR         SKY_U8(5)        /* _____101 */
+#define SKY_JSON_TYPE_ARR         SKY_U8(6)        /* _____110 */
+#define SKY_JSON_TYPE_OBJ         SKY_U8(7)        /* _____111 */
+
+/** Subtype of JSON value (2 bit). */
+#define SKY_JSON_SUBTYPE_NONE     (0 << 3) /* ___00___ */
+#define SKY_JSON_SUBTYPE_FALSE    (0 << 3) /* ___00___ */
+#define SKY_JSON_SUBTYPE_TRUE     (1 << 3) /* ___01___ */
+#define SKY_JSON_SUBTYPE_UINT     (0 << 3) /* ___00___ */
+#define SKY_JSON_SUBTYPE_INT      (1 << 3) /* ___01___ */
+#define SKY_JSON_SUBTYPE_REAL     (2 << 3) /* ___10___ */
+
+/** Mask and bits of JSON value. */
+#define SKY_JSON_TYPE_MASK        SKY_U8(0x07)     /* _____111 */
+#define SKY_JSON_TYPE_BIT         SKY_U8(3)
+#define SKY_JSON_SUBTYPE_MASK     SKY_U8(0x18)     /* ___11___ */
+#define SKY_JSON_SUBTYPE_BIT      SKY_U8(2)
+#define SKY_JSON_RESERVED_MASK    SKY_U8(0xE0)     /* 111_____ */
+#define SKY_JSON_RESERVED_BIT     SKY_U8(3)
+#define SKY_JSON_TAG_MASK         SKY_U8(0xFF)     /* 11111111 */
+#define SKY_JSON_TAG_BIT          SKY_U8(8)
+
 typedef struct sky_json_doc_s sky_json_doc_t;
 typedef struct sky_json_val_s sky_json_val_t;
 
-struct sky_json_doc_s {
-    sky_json_val_t *root;
-    sky_usize_t read_n;
-    sky_usize_t val_read_n;
-};
 
-struct sky_json_val_s {
-    sky_u64_t tag;
-    union {
-        sky_u64_t u64;
-        sky_i64_t i64;
-        void *ptr;
-        sky_f64_t f64;
-        sky_str_t str;
-    };
-};
-
-
-sky_json_doc_t *sky_json_read(sky_str_t *str);
+sky_json_doc_t *sky_json_read_opts(const sky_str_t *str, sky_u32_t opts);
 
 sky_json_val_t *sky_json_doc_get_root(sky_json_doc_t *doc);
 
-sky_json_val_t *sky_json_obj_get(sky_json_val_t *obj, sky_uchar_t *key, sky_u32_t key_len);
+sky_json_val_t *sky_json_obj_get(sky_json_val_t *obj, const sky_uchar_t *key, sky_u32_t key_len);
+
+void sky_json_doc_free(sky_json_doc_t *doc);
+
+static sky_inline sky_json_doc_t *
+sky_json_read(const sky_str_t *str, sky_u32_t opts) {
+    opts &= ~SKY_JSON_READ_IN_SITU; /* const string cannot be modified */
+
+    return sky_json_read_opts(str, opts);
+}
 
 
 
