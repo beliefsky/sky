870b9eab957ea85e97154a3ed09789f852da841b
weijing <duanweijing@deltaphone.com.cn>
2022-11-15 16:11:47 +0800

    定时任务

--- src/lib/net/un_inet.c
+++ src/lib/net/un_inet.c
@@ -4,6 +4,16 @@
 
 #include "un_inet.h"
 
+struct sky_un_inet_s {
+    sky_event_t ev;
+    sky_coro_t *coro;
+    sky_un_inet_process_pt process;
+    void *data;
+    sky_u32_t period;
+    sky_bool_t wait: 1;
+    sky_bool_t rerun: 1;
+};
+
 static sky_bool_t un_inet_run(sky_un_inet_t *un_inet);
 
 static void un_inet_close(sky_un_inet_t *un_inet);
@@ -25,17 +35,20 @@ sky_un_inet_run(
     un_inet->coro = coro;
     un_inet->process = func;
     un_inet->data = data;
+    un_inet->period = 0;
+    un_inet->wait = false;
+    un_inet->rerun = false;
 
-    sky_coro_set(coro, (sky_coro_func_t)un_inet_coro_process, un_inet);
+    sky_coro_set(coro, (sky_coro_func_t) un_inet_coro_process, un_inet);
 
     if (!un_inet_run(un_inet)) {
         un_inet_close(un_inet);
     } else {
-        sky_event_register_none(&un_inet->ev,  -1);
+        sky_event_register_none(&un_inet->ev, -1);
     }
 }
 
-void
+sky_un_inet_t *
 sky_un_inet_run_delay(
         sky_event_loop_t *ev_loop,
         sky_coro_switcher_t *switcher,
@@ -43,20 +56,66 @@ sky_un_inet_run_delay(
         void *data,
         sky_u32_t delay_sec
 ) {
-    if (sky_unlikely(!delay_sec)) {
-        sky_un_inet_run(ev_loop, switcher, func, data);
-        return;
-    }
     sky_coro_t *coro = sky_coro_new(switcher);
     sky_un_inet_t *un_inet = sky_coro_malloc(coro, sizeof(sky_un_inet_t));
     sky_event_init(ev_loop, &un_inet->ev, -1, un_inet_run, un_inet_close);
-    un_inet->ev.timer.cb = (sky_timer_wheel_pt)un_inet_delay_run;
+    un_inet->ev.timer.cb = (sky_timer_wheel_pt) un_inet_delay_run;
     un_inet->coro = coro;
     un_inet->process = func;
     un_inet->data = data;
-    sky_coro_set(coro, (sky_coro_func_t)un_inet_coro_process, un_inet);
+    un_inet->period = 0;
+    un_inet->wait = true;
+    un_inet->rerun = false;
 
+    sky_coro_set(coro, (sky_coro_func_t) un_inet_coro_process, un_inet);
     sky_event_timer_register(ev_loop, &un_inet->ev.timer, delay_sec);
+
+    return un_inet;
+}
+
+sky_un_inet_t *
+sky_un_inet_run_timer(
+        sky_event_loop_t *ev_loop,
+        sky_coro_switcher_t *switcher,
+        sky_un_inet_process_pt func,
+        void *data,
+        sky_u32_t delay_sec,
+        sky_u32_t period_sc
+) {
+    sky_coro_t *coro = sky_coro_new(switcher);
+    sky_un_inet_t *un_inet = sky_coro_malloc(coro, sizeof(sky_un_inet_t));
+    sky_event_init(ev_loop, &un_inet->ev, -1, un_inet_run, un_inet_close);
+    un_inet->ev.timer.cb = (sky_timer_wheel_pt) un_inet_delay_run;
+    un_inet->coro = coro;
+    un_inet->process = func;
+    un_inet->data = data;
+    un_inet->period = period_sc;
+    un_inet->wait = true;
+    un_inet->rerun = true;
+
+    sky_coro_set(coro, (sky_coro_func_t) un_inet_coro_process, un_inet);
+    sky_event_timer_register(ev_loop, &un_inet->ev.timer, delay_sec);
+
+    return un_inet;
+}
+
+void
+sky_un_inet_cancel(sky_un_inet_t *un_inet) {
+    if (un_inet->wait) {
+        sky_timer_wheel_unlink(&un_inet->ev.timer);
+        sky_coro_destroy(un_inet->coro);
+    }
+    un_inet->rerun = false;
+}
+
+sky_event_t *
+sky_un_inet_event(sky_un_inet_t *un_inet) {
+    return &(un_inet->ev);
+}
+
+sky_coro_t *
+sky_un_inet_coro(sky_un_inet_t *un_inet) {
+    return un_inet->coro;
 }
 
 
@@ -67,15 +126,23 @@ un_inet_run(sky_un_inet_t *un_inet) {
 
 static void
 un_inet_close(sky_un_inet_t *un_inet) {
-    sky_coro_destroy(un_inet->coro);
+    if (un_inet->rerun) {
+        un_inet->wait = true;
+        un_inet->ev.timer.cb = (sky_timer_wheel_pt) un_inet_delay_run;
+        sky_event_timer_register(un_inet->ev.loop, &un_inet->ev.timer, un_inet->period);
+        sky_coro_reset(un_inet->coro, (sky_coro_func_t) un_inet_coro_process, un_inet);
+    } else {
+        sky_coro_destroy(un_inet->coro);
+    }
 }
 
 static void
 un_inet_delay_run(sky_un_inet_t *un_inet) {
+    un_inet->wait = false;
     if (!un_inet_run(un_inet)) {
         un_inet_close(un_inet);
     } else {
-        sky_event_register_none(&un_inet->ev,  -1);
+        sky_event_register_none(&un_inet->ev, -1);
     }
 }
 
