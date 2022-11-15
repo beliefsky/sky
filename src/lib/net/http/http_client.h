ae7c9305f97b3278469d351734a0dec04e16eec7
beliefsky <ibeliefsky@gmail.com>
2022-11-15 23:08:03 +0800

    开始 http client

---
+++ src/lib/net/http/http_client.h
@@ -0,0 +1,20 @@
+//
+// Created by beliefsky on 2022/11/15.
+//
+
+#ifndef SKY_HTTP_CLIENT_H
+#define SKY_HTTP_CLIENT_H
+
+#include "../tcp_client.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+//void *sky_http_client_create(sky_event_t *event, sky_coro_t *coro);
+
+
+#if defined(__cplusplus)
+} /* extern "C" { */
+#endif
+#endif //SKY_HTTP_CLIENT_H
