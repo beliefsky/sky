0c6f73cd7f42510958a12a6722cb0515b4882fdf
weijing <duanweijing@deltaphone.com.cn>
2022-11-17 16:34:05 +0800

    http chunked

--- src/lib/net/http/http_response.h
+++ src/lib/net/http/http_response.h
@@ -17,6 +17,10 @@ void sky_http_response_static(sky_http_request_t *r, sky_str_t *buf);
 
 void sky_http_response_static_len(sky_http_request_t *r, sky_uchar_t *buf, sky_usize_t buf_len);
 
+void sky_http_response_chunked(sky_http_request_t *r, sky_str_t *buf);
+
+void sky_http_response_chunked_len(sky_http_request_t *r, sky_uchar_t *buf, sky_usize_t buf_len);
+
 void sky_http_sendfile(sky_http_request_t *r, sky_i32_t fd, sky_usize_t offset, sky_usize_t size, sky_usize_t file_size);
 
 #if defined(__cplusplus)
