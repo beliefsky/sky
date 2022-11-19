c7d54e80fca14662ee43bcdc0fd8b6a73a8c14d0
beliefsky <ibeliefsky@gmail.com>
2022-11-19 18:52:13 +0800

    http client api误删，buf的last指向可能存在的bug

--- src/lib/net/http/http_client.h
+++ src/lib/net/http/http_client.h
@@ -57,6 +57,10 @@ sky_http_client_res_t *sky_http_client_req(sky_http_client_t *client, sky_http_c
 
 sky_str_t *sky_http_client_res_body_str(sky_http_client_res_t *res);
 
+sky_bool_t sky_http_client_res_body_none(sky_http_client_res_t *res);
+
+sky_bool_t sky_http_client_res_body_file(sky_http_client_res_t *res, sky_str_t *path);
+
 void sky_http_client_destroy(sky_http_client_t *client);
 
 static sky_inline sky_bool_t
