ebf42192aaf5379a75930a02558772e6be6b3ba8
beliefsky <ibeliefsky@gmail.com>
2022-11-26 16:53:32 +0800

    none

--- src/lib/core/string.c
+++ src/lib/core/string.c
@@ -314,7 +314,9 @@ sky_str_len_find(const sky_uchar_t *src, sky_usize_t src_len, const sky_uchar_t 
         while (mask != 0) {
 
             const sky_usize_t bit_pos = (sky_usize_t) __builtin_ctz(mask);
-
+            if ((i + bit_pos) >= src_len) {
+                break;
+            }
             if (func(src + i + bit_pos + 1, sub + 1, sub_len - 2)) {
                 return (sky_uchar_t *) (src + (i + bit_pos));
             }
