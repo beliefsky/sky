6a959b7a6874b724cb0172245485b589adb17f73
beliefsky <ibeliefsky@gmail.com>
2022-12-01 22:20:52 +0800

    安全的转换

--- src/lib/core/coro.c
+++ src/lib/core/coro.c
@@ -296,8 +296,9 @@ sky_defer_t *
 sky_defer_add(sky_coro_t *coro, sky_defer_func_t func, void *data) {
     sky_defer_t *defer;
     if (!sky_queue_is_empty(&coro->free_defers)) {
-        defer = (sky_defer_t *) sky_queue_next(&coro->free_defers);
-        sky_queue_remove(&defer->link);
+        sky_queue_t *tmp = sky_queue_next(&coro->free_defers);
+        sky_queue_remove(tmp);
+        defer = sky_queue_data(tmp, sky_defer_t, link);
     } else {
         defer = sky_coro_malloc(coro, sizeof(sky_defer_t));
     }
@@ -315,8 +316,9 @@ sky_defer_t *
 sky_defer_add2(sky_coro_t *coro, sky_defer_func2_t func, void *data1, void *data2) {
     sky_defer_t *defer;
     if (!sky_queue_is_empty(&coro->free_defers)) {
-        defer = (sky_defer_t *) sky_queue_next(&coro->free_defers);
-        sky_queue_remove(&defer->link);
+        sky_queue_t *tmp = sky_queue_next(&coro->free_defers);
+        sky_queue_remove(tmp);
+        defer = sky_queue_data(tmp, sky_defer_t, link);
     } else {
         defer = sky_coro_malloc(coro, sizeof(sky_defer_t));
     }
@@ -335,8 +337,9 @@ sky_defer_t *
 sky_defer_global_add(sky_coro_t *coro, sky_defer_func_t func, void *data) {
     sky_defer_t *defer;
     if (!sky_queue_is_empty(&coro->free_defers)) {
-        defer = (sky_defer_t *) sky_queue_next(&coro->free_defers);
-        sky_queue_remove(&defer->link);
+        sky_queue_t *tmp = sky_queue_next(&coro->free_defers);
+        sky_queue_remove(tmp);
+        defer = sky_queue_data(tmp, sky_defer_t, link);
     } else {
         defer = sky_coro_malloc(coro, sizeof(sky_defer_t));
     }
@@ -355,8 +358,9 @@ sky_defer_t *
 sky_global_defer_add2(sky_coro_t *coro, sky_defer_func2_t func, void *data1, void *data2) {
     sky_defer_t *defer;
     if (!sky_queue_is_empty(&coro->free_defers)) {
-        defer = (sky_defer_t *) sky_queue_next(&coro->free_defers);
-        sky_queue_remove(&defer->link);
+        sky_queue_t *tmp = sky_queue_next(&coro->free_defers);
+        sky_queue_remove(tmp);
+        defer = sky_queue_data(tmp, sky_defer_t, link);
     } else {
         defer = sky_coro_malloc(coro, sizeof(sky_defer_t));
     }
@@ -397,16 +401,17 @@ sky_defer_remove(sky_coro_t *coro, sky_defer_t *defer) {
 sky_inline void
 sky_defer_run(sky_coro_t *coro) {
     sky_defer_t *defer;
+    sky_queue_t *tmp;
 
     while (!sky_queue_is_empty(&coro->defers)) {
-        defer = (sky_defer_t *) sky_queue_next(&coro->defers);
+        tmp = sky_queue_next(&coro->free_defers);
+        sky_queue_remove(tmp);
+        defer = sky_queue_data(tmp, sky_defer_t, link);
         defer->free = true;
-        sky_queue_remove(&defer->link);
-
         defer->one_arg ? defer->one.func(defer->one.data)
                        : defer->two.func(defer->two.data1, defer->two.data2);
 
-        sky_queue_insert_next(&coro->free_defers, &defer->link);
+        sky_queue_insert_next(&coro->free_defers, tmp);
     }
 }
 
@@ -424,7 +429,6 @@ sky_coro_malloc(sky_coro_t *coro, sky_u32_t size) {
     ptr = coro->ptr;
     coro->ptr += size;
     coro->ptr_size -= size;
-
 
     return ptr;
 }
