846e90c61958e24c97049ec173e4aae8d7b01e37
wjduan <duanweijing@deltaphone.com.cn>
2023-03-06 18:03:13 +0800

    http server使用 out stream

---
+++ src/lib/core/string_out_stream.h
@@ -0,0 +1,108 @@
+//
+// Created by weijing on 2023/3/6.
+//
+
+#ifndef SKY_STRING_OUT_STREAM_H
+#define SKY_STRING_OUT_STREAM_H
+
+#include "palloc.h"
+#include "string.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+typedef struct sky_str_out_stream_s sky_str_out_stream_t;
+
+typedef sky_bool_t (*sky_str_out_stream_pt)(void *data, const sky_uchar_t *buf, sky_usize_t size);
+
+struct sky_str_out_stream_s {
+    sky_uchar_t *start;
+    sky_uchar_t *post;
+    sky_uchar_t *end;
+    sky_pool_t *pool;
+    sky_str_out_stream_pt callback;
+    void *data;
+    sky_bool_t fail: 1;
+};
+
+
+sky_bool_t sky_str_out_stream_init(
+        sky_str_out_stream_t *stream,
+        sky_str_out_stream_pt callback,
+        void *data,
+        sky_usize_t n
+);
+
+sky_bool_t sky_str_out_stream_ini2(
+        sky_str_out_stream_t *stream,
+        sky_pool_t *pool,
+        sky_str_out_stream_pt callback,
+        void *data,
+        sky_usize_t n
+);
+
+void sky_str_out_stream_destroy(sky_str_out_stream_t *stream);
+
+sky_uchar_t *sky_str_out_stream_need_size(sky_str_out_stream_t *stream, sky_usize_t size);
+
+void sky_str_out_stream_need_commit(sky_str_out_stream_t *stream, sky_usize_t size);
+
+sky_uchar_t *sky_str_out_stream_put(sky_str_out_stream_t *stream, sky_usize_t size);
+
+void sky_str_out_stream_write_str(sky_str_out_stream_t *stream, const sky_str_t *str);
+
+void sky_str_out_stream_write_str_len(sky_str_out_stream_t *stream, const sky_uchar_t *str, sky_usize_t len);
+
+void sky_str_out_stream_write_uchar(sky_str_out_stream_t *stream, sky_uchar_t ch);
+
+void sky_str_out_stream_write_two_uchar(sky_str_out_stream_t *stream, sky_uchar_t c1, sky_uchar_t c2);
+
+void sky_str_out_stream_write_int8(sky_str_out_stream_t *stream, sky_i8_t num);
+
+void sky_str_out_stream_write_uint8(sky_str_out_stream_t *stream, sky_u8_t num);
+
+void sky_str_out_stream_write_int16(sky_str_out_stream_t *stream, sky_i16_t num);
+
+void sky_str_out_stream_write_uint16(sky_str_out_stream_t *stream, sky_u16_t num);
+
+void sky_str_out_stream_write_int32(sky_str_out_stream_t *stream, sky_i32_t num);
+
+void sky_str_out_stream_write_uint32(sky_str_out_stream_t *stream, sky_u32_t num);
+
+void sky_str_out_stream_write_int64(sky_str_out_stream_t *stream, sky_i64_t num);
+
+void sky_str_out_stream_write_uint64(sky_str_out_stream_t *stream, sky_u64_t num);
+
+void sky_str_out_stream_write_f64(sky_str_out_stream_t *stream, sky_f64_t num);
+
+void sky_str_out_stream_flush(sky_str_out_stream_t *stream);
+
+
+static sky_inline sky_bool_t
+sky_str_out_stream_fail(const sky_str_out_stream_t *stream) {
+    return stream->fail;
+}
+
+static sky_inline void
+sky_str_out_stream_reset(sky_str_out_stream_t *stream) {
+    stream->post = stream->start;
+    stream->fail = false;
+}
+
+static sky_inline const sky_uchar_t *
+sky_str_out_stream_data(sky_str_out_stream_t *stream) {
+    return stream->start;
+}
+
+static sky_inline sky_usize_t
+sky_str_out_stream_data_size(sky_str_out_stream_t *stream) {
+    return (sky_usize_t) (stream->post - stream->start);
+}
+
+
+#if defined(__cplusplus)
+} /* extern "C" { */
+#endif
+
+#endif //SKY_STRING_OUT_STREAM_H
--- src/lib/net/http/http_response.c
+++ src/lib/net/http/http_response.c
@@ -4,30 +4,27 @@
 
 #include "http_response.h"
 #include "../../core/number.h"
+#include "../../core/string_out_stream.h"
 #include "../../core/string_buf.h"
 #include "../../core/date.h"
 
-static void http_header_build(sky_http_request_t *r, sky_str_buf_t *buf);
+static void http_header_write(sky_http_request_t *r, sky_str_out_stream_t *stream);
+
+static sky_bool_t http_write_cb(void *data, const sky_uchar_t *buf, sky_usize_t size);
 
 void
 sky_http_response_nobody(sky_http_request_t *r) {
-    sky_str_buf_t str_buf;
+    sky_str_out_stream_t stream;
 
     if (sky_unlikely(r->response)) {
         return;
     }
     r->response = true;
 
-    sky_str_buf_init2(&str_buf, r->pool, 2048);
-    http_header_build(r, &str_buf);
-
-    const sky_str_t out = {
-            .len = sky_str_buf_size(&str_buf),
-            .data = str_buf.start
-    };
-    r->conn->server->http_write(r->conn, out.data, out.len);
-
-    sky_str_buf_destroy(&str_buf);
+    sky_str_out_stream_ini2(&stream, r->pool, http_write_cb, r->conn, 2048);
+    http_header_write(r, &stream);
+    sky_str_out_stream_flush(&stream);
+    sky_str_out_stream_destroy(&stream);
 }
 
 
@@ -42,7 +39,7 @@ sky_http_response_static(sky_http_request_t *r, sky_str_t *buf) {
 
 void
 sky_http_response_static_len(sky_http_request_t *r, sky_uchar_t *buf, sky_usize_t buf_len) {
-    sky_str_buf_t str_buf;
+    sky_str_out_stream_t stream;
     sky_uchar_t *data;
     sky_http_header_t *header;
 
@@ -57,49 +54,19 @@ sky_http_response_static_len(sky_http_request_t *r, sky_uchar_t *buf, sky_usize_
     if (!buf_len) {
         sky_str_set(&header->val, "0");
 
-        sky_str_buf_init2(&str_buf, r->pool, 2048);
-        http_header_build(r, &str_buf);
-
-        const sky_str_t out = {
-                .len = sky_str_buf_size(&str_buf),
-                .data = str_buf.start
-        };
-        r->conn->server->http_write(r->conn, out.data, out.len);
-
-        sky_str_buf_destroy(&str_buf);
-        return;
-    }
-
-    header->val.data = data = sky_palloc(r->pool, 16);
-    header->val.len = sky_u64_to_str(buf_len, data);
-
-    if (buf_len < 1024) {
-        sky_str_buf_init2(&str_buf, r->pool, 2048 + buf_len);
-        http_header_build(r, &str_buf);
-
-        sky_str_buf_append_str_len(&str_buf, buf, buf_len);
-
-        const sky_str_t out = {
-                .len = sky_str_buf_size(&str_buf),
-                .data = str_buf.start
-        };
-
-        r->conn->server->http_write(r->conn, out.data, out.len);
-        sky_str_buf_destroy(&str_buf);
+        sky_str_out_stream_ini2(&stream, r->pool, http_write_cb, r->conn, 2048);
+        http_header_write(r, &stream);
     } else {
-        sky_str_buf_init2(&str_buf, r->pool, 2048);
-        http_header_build(r, &str_buf);
+        header->val.data = data = sky_palloc(r->pool, 16);
+        header->val.len = sky_u64_to_str(buf_len, data);
 
-        const sky_str_t out = {
-                .len = sky_str_buf_size(&str_buf),
-                .data = str_buf.start
-        };
-
-        r->conn->server->http_write(r->conn, out.data, out.len);
-        r->conn->server->http_write(r->conn, buf, buf_len);
-
-        sky_str_buf_destroy(&str_buf);
+        sky_str_out_stream_ini2(&stream, r->pool, http_write_cb, r->conn, 2048);
+        http_header_write(r, &stream);
+        sky_str_out_stream_write_str_len(&stream, buf, buf_len);
     }
+
+    sky_str_out_stream_flush(&stream);
+    sky_str_out_stream_destroy(&stream);
 }
 
 void
@@ -113,7 +80,7 @@ sky_http_response_chunked(sky_http_request_t *r, sky_str_t *buf) {
 
 void
 sky_http_response_chunked_len(sky_http_request_t *r, sky_uchar_t *buf, sky_usize_t buf_len) {
-    sky_str_buf_t str_buf;
+    sky_str_out_stream_t stream;
     sky_http_header_t *header;
 
     if (!r->response) {
@@ -123,41 +90,35 @@ sky_http_response_chunked_len(sky_http_request_t *r, sky_uchar_t *buf, sky_usize
         sky_str_set(&header->key, "Transfer-Encoding");
         sky_str_set(&header->val, "chunked");
 
-        if (buf_len < 1024) {
-            sky_str_buf_init2(&str_buf, r->pool, 2048);
-        } else {
-            sky_str_buf_init2(&str_buf, r->pool, 2048 + buf_len);
-        }
-        http_header_build(r, &str_buf);
+        sky_str_out_stream_ini2(&stream, r->pool, http_write_cb, r->conn, 2048);
+        http_header_write(r, &stream);
+
     } else if (r->chunked) {
-        sky_str_buf_init2(&str_buf, r->pool, 20 + buf_len);
+        sky_str_out_stream_ini2(&stream, r->pool, http_write_cb, r->conn, 20 + buf_len);
     } else {
         return;
     }
     if (!buf_len) {
         r->chunked = false;
-        sky_str_buf_append_str_len(&str_buf, sky_str_line("0\r\n\r\n"));
+        sky_str_out_stream_write_str_len(&stream, sky_str_line("0\r\n\r\n"));
     } else {
-        sky_uchar_t *tmp = sky_str_buf_need_size(&str_buf, 17);
+        sky_uchar_t *tmp = sky_str_out_stream_need_size(&stream, 17);
         const sky_u8_t n = sky_usize_to_hex_str(buf_len, tmp, false);
-        sky_str_buf_need_commit(&str_buf, n);
+        sky_str_out_stream_need_commit(&stream, n);
 
-        sky_str_buf_append_two_uchar(&str_buf, '\r', '\n');
-        sky_str_buf_append_str_len(&str_buf, buf, buf_len);
-        sky_str_buf_append_two_uchar(&str_buf, '\r', '\n');
+        sky_str_out_stream_write_two_uchar(&stream, '\r', '\n');
+        sky_str_out_stream_write_str_len(&stream, buf, buf_len);
+        sky_str_out_stream_write_two_uchar(&stream, '\r', '\n');
     }
-    const sky_str_t out = {
-            .len = sky_str_buf_size(&str_buf),
-            .data = str_buf.start
-    };
-    r->conn->server->http_write(r->conn, out.data, out.len);
-    sky_str_buf_destroy(&str_buf);
+
+    sky_str_out_stream_flush(&stream);
+    sky_str_out_stream_destroy(&stream);
 }
 
 
 void
 sky_http_sendfile(sky_http_request_t *r, sky_i32_t fd, sky_usize_t offset, sky_usize_t size, sky_usize_t file_size) {
-    sky_str_buf_t str_buf;
+    sky_str_out_stream_t stream;
     sky_uchar_t *data;
     sky_http_header_t *header;
 
@@ -175,6 +136,7 @@ sky_http_sendfile(sky_http_request_t *r, sky_i32_t fd, sky_usize_t offset, sky_u
         header = sky_list_push(&r->headers_out.headers);
         sky_str_set(&header->key, "Content-Range");
 
+        sky_str_buf_t str_buf;
         sky_str_buf_init2(&str_buf, r->pool, 64);
         sky_str_buf_append_str_len(&str_buf, sky_str_line("bytes "));
         sky_str_buf_append_uint64(&str_buf, offset);
@@ -186,64 +148,71 @@ sky_http_sendfile(sky_http_request_t *r, sky_i32_t fd, sky_usize_t offset, sky_u
         sky_str_buf_build(&str_buf, &header->val);
     }
 
-    sky_str_buf_init2(&str_buf, r->pool, 2048);
-    http_header_build(r, &str_buf);
-
-    const sky_str_t out = {
-            .len = sky_str_buf_size(&str_buf),
-            .data = str_buf.start
-    };
+    sky_str_out_stream_ini2(&stream, r->pool, http_write_cb, r->conn, 2048);
+    http_header_write(r, &stream);
 
     if (size) {
-        r->conn->server->http_send_file(r->conn, fd, (sky_i64_t) offset, size, out.data, (sky_u32_t) out.len);
+        const sky_u32_t header_len = (sky_u32_t) sky_str_out_stream_data_size(&stream);
+        const sky_uchar_t *header_data = sky_str_out_stream_data(&stream);
+        r->conn->server->http_send_file(r->conn, fd, (sky_i64_t) offset, size, header_data, header_len);
+        sky_str_out_stream_reset(&stream);
     } else {
-        r->conn->server->http_write(r->conn, out.data, (sky_u32_t) out.len);
+        sky_str_out_stream_flush(&stream);
     }
-    sky_str_buf_destroy(&str_buf);
+    sky_str_out_stream_destroy(&stream);
 }
 
-
 static void
-http_header_build(sky_http_request_t *r, sky_str_buf_t *buf) {
+http_header_write(sky_http_request_t *r, sky_str_out_stream_t *stream) {
     if (!r->state) {
         r->state = 200;
     }
     const sky_http_headers_out_t *header_out = &r->headers_out;
     const sky_str_t *status = sky_http_status_find(r->conn->server, r->state);
 
-    sky_str_buf_append_str(buf, &r->version_name);
-    sky_str_buf_append_uchar(buf, ' ');
-    sky_str_buf_append_str(buf, status);
+    sky_str_out_stream_write_str(stream, &r->version_name);
+    sky_str_out_stream_write_uchar(stream, ' ');
+    sky_str_out_stream_write_str(stream, status);
 
     if (r->keep_alive) {
-        sky_str_buf_append_str_len(buf, sky_str_line("\r\nConnection: keep-alive\r\n"));
+        sky_str_out_stream_write_str_len(stream, sky_str_line("\r\nConnection: keep-alive\r\n"));
     } else {
-        sky_str_buf_append_str_len(buf, sky_str_line("\r\nConnection: close\r\n"));
+        sky_str_out_stream_write_str_len(stream, sky_str_line("\r\nConnection: close\r\n"));
     }
-    sky_str_buf_append_str_len(buf, sky_str_line("Date: "));
+    sky_str_out_stream_write_str_len(stream, sky_str_line("Date: "));
 
     if (r->conn->ev.now > r->conn->server->rfc_last) {
         sky_date_to_rfc_str(r->conn->ev.now, r->conn->server->rfc_date);
         r->conn->server->rfc_last = r->conn->ev.now;
     }
 
-    sky_str_buf_append_str_len(buf, r->conn->server->rfc_date, 29);
+    sky_str_out_stream_write_str_len(stream, r->conn->server->rfc_date, 29);
 
     if (header_out->content_type.len) {
-        sky_str_buf_append_str_len(buf, sky_str_line("\r\nContent-Type: "));
-        sky_str_buf_append_str(buf, &header_out->content_type);
+        sky_str_out_stream_write_str_len(stream, sky_str_line("\r\nContent-Type: "));
+        sky_str_out_stream_write_str(stream, &header_out->content_type);
     } else {
-        sky_str_buf_append_str_len(buf, sky_str_line("\r\nContent-Type: text/plain"));
+        sky_str_out_stream_write_str_len(stream, sky_str_line("\r\nContent-Type: text/plain"));
     }
 
-    sky_str_buf_append_str_len(buf, sky_str_line("\r\nServer: sky\r\n"));
+    sky_str_out_stream_write_str_len(stream, sky_str_line("\r\nServer: sky\r\n"));
 
     sky_list_foreach(&r->headers_out.headers, sky_http_header_t, item, {
-        sky_str_buf_append_str(buf, &item->key);
-        sky_str_buf_append_two_uchar(buf, ':', ' ');
-        sky_str_buf_append_str(buf, &item->val);
-        sky_str_buf_append_two_uchar(buf, '\r', '\n');
+        sky_str_out_stream_write_str(stream, &item->key);
+        sky_str_out_stream_write_two_uchar(stream, ':', ' ');
+        sky_str_out_stream_write_str(stream, &item->val);
+        sky_str_out_stream_write_two_uchar(stream, '\r', '\n');
     });
 
-    sky_str_buf_append_two_uchar(buf, '\r', '\n');
+    sky_str_out_stream_write_two_uchar(stream, '\r', '\n');
+}
+
+
+static sky_bool_t
+http_write_cb(void *data, const sky_uchar_t *buf, sky_usize_t size) {
+    sky_http_connection_t *conn = data;
+
+    conn->server->http_write(conn, buf, size);
+
+    return true;
 }
