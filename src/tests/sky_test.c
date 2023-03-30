283da55fae48a4573487c25712ea01a2c3b83ef2
beliefsky <ibeliefsky@gmail.com>
2023-03-30 22:54:20 +0800

    fix bug

--- src/tests/sky_test.c
+++ src/tests/sky_test.c
@@ -4,109 +4,106 @@
 #include <core/types.h>
 #include <core/log.h>
 #include <inet/http/http_server.h>
-#include <netinet/in.h>
-#include "inet/http/module/http_module_dispatcher.h"
 #include "core/array.h"
-#include "inet/http/http_response.h"
+#include "inet/http/http_client.h"
+#include "core/memory.h"
 
-static void build_http_dispatcher(sky_pool_t *pool, sky_http_module_t *module);
+typedef struct {
+    sky_ev_t ev;
+    sky_timer_wheel_entry_t timer;
+    sky_coro_t *coro;
+    sky_coro_switcher_t *switcher;
+    sky_event_loop_t *loop;
+} task_timer_t;
 
+static sky_isize_t
+coro_process(sky_coro_t *coro, void *data) {
+    task_timer_t *task = data;
 
-static SKY_HTTP_MAPPER_HANDLER(hello_world);
+    const sky_http_client_conf_t conf = {
+            .timeout = 5,
+            .keep_alive = 30
+    };
 
-sky_event_loop_t *loop;
-sky_tcp_ctx_t ctx;
+    sky_http_client_t *client = sky_http_client_create(
+            task->loop,
+            &task->ev,
+            task->coro,
+            &conf
+    );
+    if (!client) {
+        sky_log_error("client create error");
+        return SKY_CORO_ABORT;
+    }
+    sky_pool_t *pool = sky_pool_create(SKY_POOL_DEFAULT_SIZE);
+    sky_str_t url = sky_string("http://www.htmanga4.top");
+
+    sky_http_client_req_t req;
+    if (!sky_http_client_req_init(&req, pool, &url)) {
+        sky_log_error("req init error");
+        goto error;
+    }
+
+    sky_http_client_res_t *res = sky_http_client_req(client, &req);
+    if (!res) {
+        sky_log_error("res error");
+        goto error;
+    }
+    sky_str_t *body = sky_http_client_res_body_str(res);
+    if (!body) {
+        sky_log_error("res content error");
+        goto error;
+    }
+    sky_log_info("%s", body->data);
+
+    return SKY_CORO_FINISHED;
+
+    error:
+
+    sky_http_client_destroy(client);
+    sky_pool_destroy(pool);
+
+    return SKY_CORO_ABORT;
+}
 
 static void
-accept_cb(sky_tcp_t *server) {
-    sky_tcp_t client;
+timer_run(sky_ev_t *ev) {
+    task_timer_t *task = sky_type_convert(ev, task_timer_t, ev);
 
-
-    sky_tcp_init(&client, &ctx);
-    while (sky_tcp_accept(server, &client) > 0) {
-        sky_log_info("accept: %d", client.ev.fd);
-        sky_tcp_close(&client);
+    if (sky_coro_resume(task->coro) == SKY_CORO_MAY_RESUME) {
+        return;
     }
-    sky_tcp_try_register(sky_event_selector(loop), server, SKY_EV_READ);
+    sky_coro_destroy(task->coro);
+    sky_free(task);
 }
+
+static void
+timer_out(sky_timer_wheel_entry_t *timer) {
+    task_timer_t *task = sky_type_convert(timer, task_timer_t, timer);
+
+    task->coro = sky_coro_create(task->switcher, coro_process, task);
+    timer_run(&task->ev);
+}
+
 
 int
 main() {
     setvbuf(stdout, null, _IOLBF, 0);
     setvbuf(stderr, null, _IOLBF, 0);
 
+    sky_coro_switcher_t *switcher = sky_malloc(sky_coro_switcher_size());
 
-    loop = sky_event_loop_create();
-
-    sky_pool_t *pool = sky_pool_create(SKY_POOL_DEFAULT_SIZE);
-
-    sky_array_t modules;
-    sky_array_init2(&modules, pool, 8, sizeof(sky_http_module_t));
-
-    build_http_dispatcher(pool, sky_array_push(&modules));
-
-    sky_http_module_host_t hosts[] = {
-            {
-                    .host = sky_null_string,
-                    .modules = modules.elts,
-                    .modules_n = (sky_u16_t) modules.nelts
-            }
-    };
-
-    sky_http_conf_t conf = {
-            .header_buf_size = 2048,
-            .header_buf_n = 4,
-            .modules_host = hosts,
-            .modules_n = 1
-    };
-
-    sky_http_server_t *server = sky_http_server_create(loop, &conf);
-
-    struct sockaddr_in ipv4_address = {
-            .sin_family = AF_INET,
-            .sin_addr.s_addr = INADDR_ANY,
-            .sin_port = sky_htons(8080)
-    };
-    sky_http_server_bind(server, (sky_inet_addr_t *) &ipv4_address, sizeof(struct sockaddr_in));
-
-    struct sockaddr_in6 ipv6_address = {
-            .sin6_family = AF_INET6,
-            .sin6_addr = in6addr_any,
-            .sin6_port = sky_htons(8080)
-    };
-
-    sky_http_server_bind(server, (sky_inet_addr_t *) &ipv6_address, sizeof(struct sockaddr_in6));
-
+    sky_event_loop_t *loop = sky_event_loop_create();
+    task_timer_t *timer = sky_malloc(sizeof(task_timer_t));
+    timer->loop = loop;
+    timer->coro = null;
+    timer->switcher = switcher;
+    sky_ev_init(&timer->ev, timer_run, -1);
+    sky_timer_entry_init(&timer->timer, timer_out);
+    sky_event_timeout_set(loop, &timer->timer, 1);
 
     sky_event_loop_run(loop);
     sky_event_loop_destroy(loop);
 
     return 0;
-}
-
-static void
-build_http_dispatcher(sky_pool_t *pool, sky_http_module_t *module) {
-    sky_http_mapper_t mappers[] = {
-            {
-                    .path = sky_string("/hello"),
-                    .get_handler = hello_world
-            }
-    };
-
-    const sky_http_dispatcher_conf_t conf = {
-            .prefix = sky_string("/api"),
-            .mappers = mappers,
-            .mapper_len = 1,
-            .module = module
-    };
-
-    sky_http_module_dispatcher_init(pool, &conf);
-}
-
-static SKY_HTTP_MAPPER_HANDLER(hello_world) {
-    sky_http_response_static_len(req, sky_str_line("{\"status\": 200, \"msg\": \"success\"}"));
-//    sky_http_res_chunked_t *chunked = sky_http_response_chunked_start(req);
-//
-//    sky_http_response_chunked_write_len(chunked, sky_str_line("{\"status\": 200, \"msg\": \"success\"}"));
-//    sky_http_response_chunked_end(chunked);
 }
