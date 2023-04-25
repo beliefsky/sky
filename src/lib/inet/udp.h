3d85b92397172efd0446fc3ac5c9e9d862f36e1d
wjduan <duanweijing@deltaphone.com.cn>
2023-04-25 10:45:28 +0800

    udp报文收发

--- src/lib/inet/udp.h
+++ src/lib/inet/udp.h
@@ -16,10 +16,19 @@ typedef struct sky_udp_s sky_udp_t;
 
 typedef void (*sky_udp_cb_pt)(sky_udp_t *udp);
 
+typedef sky_isize_t (*sky_udp_read_pt)(sky_udp_t *udp, sky_inet_addr_t *addr, sky_uchar_t *data, sky_usize_t size);
+
+typedef sky_bool_t (*sky_udp_write_pt)(
+        sky_udp_t *udp,
+        const sky_inet_addr_t *addr,
+        const sky_uchar_t *data,
+        sky_usize_t size
+);
+
 struct sky_udp_ctx_s {
     sky_udp_cb_pt close;
-//    sky_udp_read_pt read;
-//    sky_udp_write_pt write;
+    sky_udp_read_pt read;
+    sky_udp_write_pt write;
     void *ex_data;
 };
 
@@ -37,6 +46,51 @@ void sky_udp_init(sky_udp_t *udp, sky_udp_ctx_t *ctx, sky_selector_t *s);
 sky_bool_t sky_udp_open(sky_udp_t *udp, sky_i32_t domain);
 
 sky_bool_t sky_udp_bind(sky_udp_t *udp, const sky_inet_addr_t *addr);
+
+sky_isize_t sky_udp_read(sky_udp_t *udp, sky_inet_addr_t *addr, sky_uchar_t *data, sky_usize_t size);
+
+sky_bool_t sky_udp_write(sky_udp_t *udp, const sky_inet_addr_t *addr, const sky_uchar_t *data, sky_usize_t size);
+
+
+void sky_udp_close(sky_udp_t *udp);
+
+static sky_inline sky_ev_t *
+sky_udp_ev(sky_udp_t *udp) {
+    return &udp->ev;
+}
+
+static sky_inline sky_socket_t
+sky_udp_fd(sky_udp_t *udp) {
+    return sky_ev_get_fd(&udp->ev);
+}
+
+static sky_inline void
+sky_udp_set_cb(sky_udp_t *udp, sky_udp_cb_pt cb) {
+    sky_ev_reset_cb(&udp->ev, (sky_ev_cb_pt) cb);
+}
+
+static sky_inline sky_bool_t
+sky_udp_register(sky_udp_t *udp) {
+    return sky_selector_register(&udp->ev, SKY_EV_READ);
+}
+
+static sky_inline sky_bool_t
+sky_udp_try_register(sky_udp_t *udp) {
+    if (sky_ev_reg(&udp->ev)) {
+        return true;
+    }
+    return sky_selector_register(&udp->ev, SKY_EV_READ);
+}
+
+static sky_inline sky_bool_t
+sky_udp_register_update(sky_udp_t *udp) {
+    return sky_selector_update(&udp->ev, SKY_EV_READ);
+}
+
+static sky_inline sky_bool_t
+sky_udp_register_cancel(sky_udp_t *udp) {
+    return sky_selector_cancel(&udp->ev);
+}
 
 static sky_inline sky_bool_t
 sky_udp_is_closed(const sky_udp_t *udp) {
