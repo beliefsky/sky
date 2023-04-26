c109c45b332b2cc59da142c2c977e4b405859186
beliefsky <ibeliefsky@gmail.com>
2023-04-26 23:57:21 +0800

    开始dns解析

--- src/lib/inet/dns/dns_protocol.c
+++ src/lib/inet/dns/dns_protocol.c
@@ -5,6 +5,7 @@
 #include "dns_protocol.h"
 #include "../../core/memory.h"
 #include "../inet.h"
+#include "../../core/log.h"
 
 sky_u32_t
 sky_dns_encode_size(const sky_dns_packet_t *packet) {
@@ -66,6 +67,67 @@ sky_dns_encode(const sky_dns_packet_t *packet, sky_uchar_t *buf) {
 
 
     return n;
+}
+
+sky_bool_t
+sky_dns_decode_header(sky_dns_packet_t *packet, const sky_uchar_t *buf, sky_u32_t size) {
+    if (sky_unlikely(size < 12)) {
+        return false;
+    }
+    sky_dns_header_t *header = &packet->header;
+
+    header->id = sky_htons(*(sky_u16_t *) buf);
+    buf += 2;
+    header->flags = sky_htons(*(sky_u16_t *) buf);
+    buf += 2;
+    header->qd_count = sky_htons(*(sky_u16_t *) buf);
+    buf += 2;
+    header->an_count = sky_htons(*(sky_u16_t *) buf);
+    buf += 2;
+    header->ns_count = sky_htons(*(sky_u16_t *) buf);
+    buf += 2;
+    header->ar_count = sky_htons(*(sky_u16_t *) buf);
+
+    return true;
+}
+
+sky_bool_t
+sky_dns_decode_body(sky_dns_packet_t *packet, sky_uchar_t *buf, sky_u32_t size) {
+    sky_u8_t n;
+    sky_u16_t i;
+
+    buf += 12;
+    size -= 12;
+
+    {
+        sky_dns_question_t *question = packet->questions;
+        for (i = packet->header.qd_count; i > 0; --i, ++question) {
+            question->name = buf;
+            question->name_len = 0;
+
+            for (;;) {
+                n = *buf;
+                if (n > 0) {
+                    *buf++ = '.';
+                    buf += n++;
+
+                    question->name_len += n;
+                    size -= n;
+                    continue;
+                }
+                ++buf;
+                ++question->name;
+                --question->name_len;
+                break;
+            }
+            question->type = sky_htons(*(sky_u16_t *) buf);
+            buf += 2;
+            question->clazz = sky_htons(*(sky_u16_t *) buf);
+            buf += 2;
+        }
+    }
+
+    return true;
 }
 
 
