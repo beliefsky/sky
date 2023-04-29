ee1aee7005180079b2367d29389b43d24a3edece
beliefsky <ibeliefsky@gmail.com>
2023-04-29 17:46:22 +0800

    none

--- src/lib/inet/dns/dns_protocol.c
+++ src/lib/inet/dns/dns_protocol.c
@@ -7,21 +7,23 @@
 #include "../inet.h"
 #include "../../core/log.h"
 
+
+typedef struct {
+    sky_uchar_t *start;
+    sky_uchar_t **buf_ref;
+    sky_u32_t *size_ref;
+    sky_u32_t size;
+} buf_ref_t;
+
 static sky_bool_t decode_question(
         sky_dns_question_t *q,
-        sky_uchar_t **b_ptr,
-        sky_uchar_t *start,
-        sky_u32_t *s_ptr,
-        sky_u32_t start_size,
+        buf_ref_t *ref,
         sky_u16_t qn
 );
 
 static sky_bool_t decode_answer(
         sky_dns_answer_t *a,
-        sky_uchar_t **b_ptr,
-        sky_uchar_t *start,
-        sky_u32_t *s_ptr,
-        sky_u32_t start_size,
+        buf_ref_t *ref,
         sky_u16_t an
 );
 
@@ -124,25 +126,30 @@ sky_dns_decode_header(sky_dns_packet_t *packet, const sky_uchar_t *buf, sky_u32_
 
 sky_bool_t
 sky_dns_decode_body(sky_dns_packet_t *packet, sky_uchar_t *buf, sky_u32_t size) {
-    const sky_u32_t buf_size = size;
-    sky_uchar_t *start = buf;
+    buf_ref_t ref = {
+            .start = buf,
+            .buf_ref = &buf,
+            .size_ref = &size,
+            .size = size
+    };
 
     buf += 12;
     size -= 12;
 
-    if (sky_unlikely(!decode_question(packet->questions, &buf, start, &size, buf_size, packet->header.qd_count))) {
+
+    if (sky_unlikely(!decode_question(packet->questions, &ref, packet->header.qd_count))) {
         sky_log_warn("decode question error");
         return false;
     }
-    if (sky_unlikely(!decode_answer(packet->answers, &buf, start, &size, buf_size, packet->header.an_count))) {
+    if (sky_unlikely(!decode_answer(packet->answers, &ref, packet->header.an_count))) {
         sky_log_warn("decode answers error");
         return false;
     }
-    if (sky_unlikely(!decode_answer(packet->authorities, &buf, start, &size, buf_size, packet->header.ns_count))) {
+    if (sky_unlikely(!decode_answer(packet->authorities, &ref, packet->header.ns_count))) {
         sky_log_warn("decode authorities error");
         return false;
     }
-    if (sky_unlikely(!decode_answer(packet->additional, &buf, start, &size, buf_size, packet->header.ar_count))) {
+    if (sky_unlikely(!decode_answer(packet->additional, &ref, packet->header.ar_count))) {
         sky_log_warn("decode additional error");
         return false;
     }
@@ -154,20 +161,17 @@ sky_dns_decode_body(sky_dns_packet_t *packet, sky_uchar_t *buf, sky_u32_t size) 
 static sky_bool_t
 decode_question(
         sky_dns_question_t *q,
-        sky_uchar_t **b_ptr,
-        sky_uchar_t *start,
-        sky_u32_t *s_ptr,
-        sky_u32_t start_size,
+        buf_ref_t *ref,
         sky_u16_t qn
 ) {
-    sky_u32_t size = *s_ptr;
-    sky_uchar_t *buf = *b_ptr;
+    sky_u32_t size = *ref->size_ref;
+    sky_uchar_t *buf = *ref->buf_ref;
 
     for (; qn > 0; --qn) {
         if (sky_unlikely(size < 4)) {
             return false;
         }
-        if (sky_unlikely(!decode_name(start, &buf, &size, start_size))) {
+        if (sky_unlikely(!decode_name(ref->start, &buf, &size, ref->size))) {
             return false;
         }
         if (sky_unlikely(size < 4)) {
@@ -182,8 +186,8 @@ decode_question(
         size -= 4;
     }
 
-    *b_ptr = buf;
-    *s_ptr = size;
+    *ref->buf_ref = buf;
+    *ref->size_ref = size;
 
     return true;
 }
@@ -191,47 +195,77 @@ decode_question(
 static sky_bool_t
 decode_answer(
         sky_dns_answer_t *a,
-        sky_uchar_t **b_ptr,
-        sky_uchar_t *start,
-        sky_u32_t *s_ptr,
-        sky_u32_t start_size,
+        buf_ref_t *ref,
         sky_u16_t an
 ) {
-    sky_u32_t size = *s_ptr;
-    sky_uchar_t *buf = *b_ptr;
+    sky_u32_t size = *ref->size_ref;
+    sky_uchar_t *buf = *ref->buf_ref;
 
     for (; an > 0; --an) {
         if (sky_unlikely(size < 10)) {
             return false;
         }
-        if (sky_unlikely(!decode_name(start, &buf, &size, start_size))) {
+
+        if (sky_unlikely(!decode_name(ref->start, &buf, &size, ref->size))) {
             return false;
         }
 
         if (sky_unlikely(size < 10)) {
             return false;
         }
-        sky_log_info("answer type: %d", sky_htons(*(sky_u16_t *) buf));
+        a->type =  sky_htons(*(sky_u16_t *) buf);
+        sky_log_info("answer type: %d", a->type);
         buf += 2;
-        sky_log_info("answer clazz: %d", sky_htons(*(sky_u16_t *) buf));
+        a->clazz =  sky_htons(*(sky_u16_t *) buf);
+        sky_log_info("answer clazz: %d", a->clazz);
         buf += 2;
-        sky_log_info("answer ttl: %u", sky_htonl(*(sky_u32_t *) buf));
+        a->ttl =  sky_htonl(*(sky_u32_t *) buf);
+        sky_log_info("answer ttl: %u", a->ttl);
         buf += 4;
 
-        const sky_u16_t data_len = sky_htons(*(sky_u16_t *) buf);
-
-        sky_log_info("answer data len: %d", data_len);
+        a->resource_len = sky_htons(*(sky_u16_t *) buf);
+        sky_log_info("answer data len: %d", a->resource_len);
         buf += 2;
-
         size -= 10;
 
-        print_buf(buf, data_len);
-        buf += data_len;
-        size -= data_len;
+        if (sky_unlikely(size < a->resource_len)) {
+            return false;
+        }
+        sky_u32_t resource_len = a->resource_len;
+        sky_uchar_t *resource = buf;
+
+        buf += resource_len;
+        size -= resource_len;
+
+        switch (a->type) {
+            case 1:
+                sky_log_info("%d.%d.%d.%d", resource[0], resource[1], resource[2], resource[3]);
+                break;
+            case 5: {
+                if (sky_unlikely(!decode_name(ref->start, &resource, &resource_len, ref->size) || resource_len)) {
+                    return false;
+                }
+                break;
+            }
+            default: {
+                if (sky_unlikely(!decode_name(ref->start, &resource, &resource_len, ref->size) || !resource_len)) {
+                    return false;
+                }
+                if (sky_unlikely(!decode_name(ref->start, &resource, &resource_len, ref->size) || !resource_len)) {
+                    return false;
+                }
+                print_buf(resource, resource_len);
+
+                break;
+            }
+        }
+
+
+        sky_log_info("===============================");
     }
 
-    *b_ptr = buf;
-    *s_ptr = size;
+    *ref->buf_ref = buf;
+    *ref->size_ref = size;
 
     return true;
 }
