78b03027f7f298b753964f071aa6c9b04a2a1a22
beliefsky <ibeliefsky@gmail.com>
2023-07-31 23:36:09 +0800

    none

--- lib/io/http/server_req_body_chunked.c
+++ lib/io/http/server_req_body_chunked.c
@@ -4,17 +4,29 @@
 #include "http_server_common.h"
 
 void
-http_req_body_chunked_none(sky_http_server_request_t *r, sky_http_server_next_pt call, void *data) {
+http_req_body_chunked_none(
+        sky_http_server_request_t *const r,
+        const sky_http_server_next_pt call,
+        void *const data
+) {
 
 }
 
 void
-http_req_body_chunked_str(sky_http_server_request_t *r, sky_http_server_next_str_pt call, void *data) {
+http_req_body_chunked_str(
+        sky_http_server_request_t *const r,
+        const sky_http_server_next_str_pt call,
+        void *const data
+) {
 
 }
 
 void
-http_req_body_chunked_read(sky_http_server_request_t *r, sky_http_server_next_read_pt call, void *data) {
+http_req_body_chunked_read(
+        sky_http_server_request_t *const r,
+        const sky_http_server_next_read_pt call,
+        void *const data
+) {
 
 }
 
