35c9ae10df18381f61244fd610518698aa56ff0b
wjduan <duanweijing@deltaphone.com.cn>
2023-08-11 09:23:38 +0800

    chunked req read none

--- lib/io/http/server_req_body_chunked.c
+++ lib/io/http/server_req_body_chunked.c
@@ -3,7 +3,13 @@
 //
 #include "http_server_common.h"
 #include <core/hex.h>
-#include <core/log.h>
+#include <core/memory.h>
+
+
+static void http_body_read_none(sky_tcp_t *tcp);
+
+static void http_read_body_none_timeout(sky_timer_wheel_entry_t *entry);
+
 
 void
 http_req_body_chunked_none(
@@ -13,24 +19,78 @@ http_req_body_chunked_none(
 ) {
     sky_http_connection_t *const conn = r->conn;
     sky_buf_t *const buf = conn->buf;
-    const sky_usize_t read_n = (sky_usize_t) (buf->last - buf->pos);
-    if (read_n >= 18) {
-        sky_isize_t n = sky_str_len_index_char(buf->pos, 18, '\n');
+    sky_uchar_t *p = buf->pos;
+
+    sky_usize_t read_n = (sky_usize_t) (buf->last - buf->pos);
+    r->headers_in.content_length_n = 0;
+
+    next_chunked:
+    if (read_n > 2) {
+        sky_isize_t n;
+        if (read_n < 18) {
+            n = sky_str_len_index_char(p, read_n, '\n');
+            if (n < 0) {
+                r->index = read_n;
+                if (buf->pos != p) {
+                    sky_memmove(buf->pos, p, read_n);
+                    buf->last = buf->pos + read_n;
+                }
+                goto next_read;
+            }
+        } else {
+            n = sky_str_len_index_char(p, 18, '\n');
+        }
         if (sky_unlikely(n < 2
-                         || buf->pos[--n] != '\r'
-                         || !sky_hex_str_len_to_usize(buf->pos, (sky_usize_t) n, &r->headers_in.content_length_n))) {
+                         || p[--n] != '\r'
+                         || !sky_hex_str_len_to_usize(p, (sky_usize_t) n, &r->headers_in.content_length_n))) {
             goto error;
         }
+        r->index = r->headers_in.content_length_n == 0;
+        r->headers_in.content_length_n += 2;
+        p += n + 2;
+        read_n -= (sky_usize_t) n + 2;
 
-        sky_log_info("%lu", r->headers_in.content_length_n);
+        if (read_n >= r->headers_in.content_length_n) {
+            p += r->headers_in.content_length_n;
+            if (sky_unlikely(!sky_str2_cmp(p - 2, '\r', '\n'))) {
+                goto error;
+            }
+            r->headers_in.content_length_n = 0;
+            if (r->index) { //end
+                buf->pos = p;
+                sky_buf_rebuild(buf, 0);
+                call(r, data);
+                return;
+            }
+            goto next_chunked;
+        }
+        p += r->headers_in.content_length_n;
+        r->headers_in.content_length_n -= read_n;
+        buf->last = buf->pos;
+        if (sky_unlikely(r->headers_in.content_length_n == 1)) { // 防止\r\n不完整
+            *(buf->last++) = *(p - 1);
+        }
     }
+
+    next_read:
+    if ((sky_usize_t) (buf->end - buf->pos) < SKY_USIZE(4096)) {
+        sky_buf_rebuild(buf, SKY_USIZE(8192));
+    }
+    r->req_pos = buf->last;
+
+    sky_timer_set_cb(&conn->timer, http_read_body_none_timeout);
+    conn->next_cb = call;
+    conn->cb_data = data;
+    sky_tcp_set_cb(&conn->tcp, http_body_read_none);
+    http_body_read_none(&conn->tcp);
+
     return;
 
     error:
     sky_tcp_close(&conn->tcp);
     sky_buf_rebuild(conn->buf, 0);
     r->error = true;
-    conn->next_cb(r, conn->cb_data);
+    call(r, data);
 }
 
 void
@@ -49,5 +109,144 @@ http_req_body_chunked_read(
         void *const data
 ) {
 
+}
+
+static void
+http_body_read_none(sky_tcp_t *const tcp) {
+    sky_http_connection_t *const conn = sky_type_convert(tcp, sky_http_connection_t, tcp);
+    sky_http_server_request_t *const req = conn->current_req;
+    sky_buf_t *const buf = conn->buf;
+
+    sky_isize_t n;
+
+    read_again:
+    n = sky_tcp_read(tcp, buf->last, (sky_usize_t) (buf->end - buf->last));
+    if (n > 0) {
+        buf->last += n;
+        if (req->headers_in.content_length_n != 0) { // 应该读取body部分
+            if ((sky_usize_t) n < req->headers_in.content_length_n) {
+                req->headers_in.content_length_n -= (sky_usize_t) n;
+                if (sky_unlikely(req->headers_in.content_length_n == 1)) { // 防止\r\n不完整
+                    *(buf->pos + 1) = *(buf->last - 1);
+                    buf->last = buf->pos + 1;
+                } else {
+                    buf->last = buf->pos;
+                }
+                goto read_again;
+            }
+            const sky_usize_t read_n = (sky_usize_t) n - req->headers_in.content_length_n;
+
+            req->req_pos = buf->last - read_n;
+
+            if (sky_unlikely(!sky_str2_cmp(req->req_pos - 2, '\r', '\n'))) {
+                goto error;
+            }
+            req->headers_in.content_length_n = 0;
+            if (req->index) { //end
+                buf->pos = req->req_pos;
+                req->headers_in.content_length_n = 0;
+                sky_buf_rebuild(buf, 0);
+                conn->next_cb(req, conn->cb_data);
+                return;
+            }
+            req->index = 0;
+
+            const sky_usize_t free_size = (sky_usize_t) (buf->end - req->req_pos);
+            if (free_size < 18) {
+                sky_memmove(buf->pos, req->req_pos, read_n);
+                buf->last = buf->pos + read_n;
+                req->req_pos = buf->pos;
+            }
+        }
+
+        sky_usize_t read_n;
+
+        next_chunked:
+        read_n = (sky_usize_t) (buf->last - req->req_pos);
+        n = sky_str_len_index_char(req->req_pos, read_n, '\n');
+        if (n < 0) {
+            req->index += read_n;
+            req->req_pos += read_n;
+            if (sky_unlikely(req->index >= 18)) {
+                goto error;
+            }
+            goto read_again;
+        }
+        if (sky_unlikely(req->req_pos[--n] != '\r'
+                         || !sky_hex_str_len_to_usize(
+                req->req_pos - req->index,
+                (sky_usize_t) n + req->index,
+                &req->headers_in.content_length_n
+        ))) {
+            goto error;
+        }
+        req->index = req->headers_in.content_length_n == 0;
+        req->headers_in.content_length_n += 2;
+        req->req_pos += n + 2;
+        read_n -= (sky_usize_t) n + 2;
+
+        if (read_n >= req->headers_in.content_length_n) {
+            req->req_pos += req->headers_in.content_length_n;
+            read_n -= req->headers_in.content_length_n;
+            if (sky_unlikely(!sky_str2_cmp(req->req_pos - 2, '\r', '\n'))) {
+                goto error;
+            }
+            req->headers_in.content_length_n = 0;
+            if (req->index) { //end
+                buf->pos = req->req_pos;
+                sky_buf_rebuild(buf, 0);
+                conn->next_cb(req, conn->cb_data);
+                return;
+            }
+            req->index = 0;
+            if (read_n >= 18) {
+                goto next_chunked;
+            }
+            const sky_usize_t free_size = (sky_usize_t) (buf->end - req->req_pos);
+            if (free_size >= 18) {
+                goto next_chunked;
+            }
+            sky_memmove(buf->pos, req->req_pos, read_n);
+            buf->last = buf->pos + read_n;
+            req->req_pos = buf->pos;
+
+            goto next_chunked;
+        }
+
+        req->headers_in.content_length_n -= read_n;
+        if (sky_unlikely(req->headers_in.content_length_n == 1)) { // 防止\r\n不完整
+            *(buf->pos + 1) = *(buf->last - 1);
+            buf->last = buf->pos + 1;
+        } else {
+            buf->last = buf->pos;
+        }
+        goto read_again;
+    }
+
+    if (sky_likely(!n)) {
+        sky_tcp_try_register(&conn->tcp, SKY_EV_READ | SKY_EV_WRITE);
+        sky_event_timeout_set(conn->ev_loop, &conn->timer, conn->server->timeout);
+        return;
+    }
+
+    error:
+    sky_timer_wheel_unlink(&conn->timer);
+    sky_tcp_close(&conn->tcp);
+    sky_buf_rebuild(conn->buf, 0);
+    req->headers_in.content_length_n = 0;
+    req->error = true;
+    conn->next_cb(req, conn->cb_data);
+}
+
+static void
+http_read_body_none_timeout(sky_timer_wheel_entry_t *const entry) {
+    sky_http_connection_t *const conn = sky_type_convert(entry, sky_http_connection_t, timer);
+    sky_http_server_request_t *const req = conn->current_req;
+
+    sky_tcp_close(&conn->tcp);
+    sky_buf_rebuild(conn->buf, 0);
+    req->headers_in.content_length_n = 0;
+    req->error = true;
+    conn->next_cb(req, conn->cb_data);
 }
 
