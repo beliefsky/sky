4a44e0a957c1a9794d82ca8514ff8b5db2c6c599
wjduan <duanweijing@deltaphone.com.cn>
2024-04-30 11:26:56 +0800

    iocp静默时使用单个事件函数

--- lib/io/ev_loop/win/iocp.c
+++ lib/io/ev_loop/win/iocp.c
@@ -13,6 +13,8 @@
 
 #define IOCP_EVENT_NUM 1024
 
+static DWORD run_pending(sky_ev_loop_t *ev_loop);
+
 sky_api sky_ev_loop_t *
 sky_ev_loop_create() {
     WSADATA ws;
@@ -44,57 +46,60 @@ sky_ev_loop_run(sky_ev_loop_t *ev_loop) {
             [EV_REQ_TCP_READ] = event_on_tcp_read
     };
 
-
-    DWORD timeout;
-    ULONG n;
+    DWORD bytes, timeout;
+    ULONG i, n;
+    ULONG_PTR key;
+    LPOVERLAPPED pov;
     LPOVERLAPPED_ENTRY event;
     sky_ev_req_t *req;
-    sky_ev_t *ev, *next;
-    sky_u64_t next_time;
+    sky_ev_t *ev;
 
     for (;;) {
-        sky_timer_wheel_run(ev_loop->timer_ctx, 0);
-        if (ev_loop->pending) {
-            do {
-                ev = ev_loop->pending;
-                ev_loop->pending = null;
-                ev_loop->pending_tail = &ev_loop->pending;
-                do {
-                    next = ev->next;
-                    ev->cb(ev);
-                    ev = next;
-                } while (ev);
-
-            } while (ev_loop->pending);
-        }
-        next_time = sky_timer_wheel_timeout(ev_loop->timer_ctx);
-        timeout = next_time == SKY_U64_MAX ? INFINITE : (DWORD) next_time;
-
-        if (!GetQueuedCompletionStatusEx(
-                ev_loop->iocp,
-                ev_loop->sys_evs,
-                IOCP_EVENT_NUM,
-                &n,
-                timeout,
-                false
-        )) {
-            if (sky_likely(GetLastError() == WAIT_TIMEOUT)) {
+        timeout = run_pending(ev_loop);
+        if (GetQueuedCompletionStatus(ev_loop->iocp, &bytes, &key, &pov, timeout)) {
+            ev = (sky_ev_t *) key;
+            req = (sky_ev_req_t *) pov;
+            EVENT_TABLES[req->type](ev, req, bytes, true);
+        } else {
+            if (GetLastError() == WAIT_TIMEOUT) {
                 continue;
             }
-            return;
+            if (sky_unlikely(!pov)) {
+                return;
+            }
+            ev = (sky_ev_t *) key;
+            req = (sky_ev_req_t *) pov;
+            EVENT_TABLES[req->type](ev, req, bytes, false);
         }
-        if (!n) {
-            continue;
-        }
-        event = ev_loop->sys_evs;
-        do {
-            ev = (sky_ev_t *) event->lpCompletionKey;
-            req = (sky_ev_req_t *) event->lpOverlapped;
-            EVENT_TABLES[req->type](ev, req, event->dwNumberOfBytesTransferred, !event->Internal);
-            ++event;
-        } while ((--n));
-    }
 
+        do {
+            timeout = run_pending(ev_loop);
+            if (!GetQueuedCompletionStatusEx(
+                    ev_loop->iocp,
+                    ev_loop->sys_evs,
+                    IOCP_EVENT_NUM,
+                    &n,
+                    timeout,
+                    false
+            )) {
+                if (sky_likely(GetLastError() == WAIT_TIMEOUT)) {
+                    break;
+                }
+                return;
+            }
+            if (!n) {
+                break;
+            }
+            i = n;
+            event = ev_loop->sys_evs;
+            do {
+                ev = (sky_ev_t *) event->lpCompletionKey;
+                req = (sky_ev_req_t *) event->lpOverlapped;
+                EVENT_TABLES[req->type](ev, req, event->dwNumberOfBytesTransferred, !event->Internal);
+                ++event;
+            } while ((--i));
+        } while (n == IOCP_EVENT_NUM);
+    }
 }
 
 sky_api void
@@ -107,6 +112,29 @@ sky_ev_loop_stop(sky_ev_loop_t *ev_loop) {
 sky_api sky_inline void
 sky_ev_timeout_init(sky_ev_loop_t *ev_loop, sky_timer_wheel_entry_t *timer, sky_timer_wheel_pt cb) {
     sky_timer_entry_init(timer, ev_loop->timer_ctx, cb);
+}
+
+
+static DWORD
+run_pending(sky_ev_loop_t *ev_loop) {
+    sky_ev_t *ev, *next;
+
+    sky_timer_wheel_run(ev_loop->timer_ctx, 0);
+    if (ev_loop->pending) {
+        do {
+            ev = ev_loop->pending;
+            ev_loop->pending = null;
+            ev_loop->pending_tail = &ev_loop->pending;
+            do {
+                next = ev->next;
+                ev->cb(ev);
+                ev = next;
+            } while (ev);
+
+        } while (ev_loop->pending);
+    }
+    const sky_u64_t next_time = sky_timer_wheel_timeout(ev_loop->timer_ctx);
+    next_time == SKY_U64_MAX ? INFINITE : (DWORD) next_time;
 }
 
 #endif
