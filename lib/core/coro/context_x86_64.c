7dfffd53756cc99cf0210aee1f20e90f406c24c7
wjduan <duanweijing@deltaphone.com.cn>
2024-05-07 09:16:41 +0800

    none

--- lib/core/coro/context_x86_64.c
+++ lib/core/coro/context_x86_64.c
@@ -54,7 +54,7 @@ asm(".text\n\t"
 asm(".text\n\t"
     ".p2align 4\n\t"
     ASM_ROUTINE(coro_entry_point_x86_64)
-    "mov %r15, %rdx\n\t"
+    "movq %r15, %rdx\n\t"
     "jmp " ASM_SYMBOL(coro_entry_point) "\n\t"
 );
 
