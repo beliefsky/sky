5ebde44ffff998b532d270a3f92f4e76e8f954f4
wjduan <duanweijing@deltaphone.com.cn>
2024-06-06 16:29:55 +0800

    freebsd sendfile flags设置问题

--- lib/io/core/unix/tcp/tcp_client.c
+++ lib/io/core/unix/tcp/tcp_client.c
@@ -1003,7 +1003,7 @@ tcp_sendfile(const sky_tcp_cli_t *const cli, const sky_tcp_fs_data_t *const data
                 data->size,
                 null,
                 &write_n,
-                SF_MNOWAIT
+                SF_NODISKIO
         );
     } else {
         struct sf_hdtr hdtr = {
@@ -1019,7 +1019,7 @@ tcp_sendfile(const sky_tcp_cli_t *const cli, const sky_tcp_fs_data_t *const data
                 data->size,
                 &hdtr,
                 &write_n,
-                SF_MNOWAIT
+                SF_NODISKIO
         );
     }
     if (r == 0) {
